a1bcf90c462461a55399bccba643a1c3721888e6
tungpham <phamthanhtung1995@gmail.com>
2020-05-24 21:33:31 +0700

    update corner radius

--- lib/widgets/src/kaylee_image_picker.dart
+++ lib/widgets/src/kaylee_image_picker.dart
@@ -47,21 +47,21 @@ class _KayleeImagePickerState extends BaseState<KayleeImagePicker> {
             height: Dimens.px103,
             child: widget.image.isNullOrEmpty && file.isNull
                 ? Center(
-              child: Image.asset(
-                Images.ic_image_holder,
-                width: Dimens.px40,
-                height: Dimens.px40,
-              ),
-            )
+                    child: Image.asset(
+                      Images.ic_image_holder,
+                      width: Dimens.px40,
+                      height: Dimens.px40,
+                    ),
+                  )
                 : AspectRatio(
-              aspectRatio: 1,
-              child: file.isNotNull
-                  ? Image.file(file)
-                  : Image.network(
-                widget.image ?? '',
-                fit: BoxFit.cover,
-              ),
-            ),
+                    aspectRatio: 1,
+                    child: file.isNotNull
+                        ? Image.file(file)
+                        : Image.network(
+                            widget.image ?? '',
+                            fit: BoxFit.cover,
+                          ),
+                  ),
           ),
           Container(
             height: Dimens.px56,
@@ -73,7 +73,7 @@ class _KayleeImagePickerState extends BaseState<KayleeImagePicker> {
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(Dimens.px10)),
               onPressed: () {
-               showImagePickerDialog();
+                showImagePickerDialog();
               },
               child: Row(
                 children: [
@@ -104,72 +104,70 @@ class _KayleeImagePickerState extends BaseState<KayleeImagePicker> {
     );
   }
 
-   showImagePickerDialog() {
-     showModalBottomSheet(
-         context: context,
-         shape: const RoundedRectangleBorder(
-             borderRadius: BorderRadius.only(
-                 topLeft: Radius.circular(Dimens.px5),
-                 topRight: Radius.circular(Dimens.px5))),
-         enableDrag: true,
-         backgroundColor: Colors.transparent,
-         isScrollControlled: true,
-         builder: (context) {
-           final width = (screenSize.width - 4 * Dimens.px16) / 3
-           return GestureDetector(
-             onTap: () {
-               pop(PageIntent(context, null));
-             },
-             child: Container(
-               color: Colors.transparent,
-               child: DraggableScrollableSheet(
-                 maxChildSize: 1,
-                 expand: true,
-                 builder: (c, scrollController) {
-                   return Container(
-                     decoration: BoxDecoration(
-                       color: Colors.white,
-                       borderRadius: BorderRadius.circular(
-                           Dimens.px5),
-                       boxShadow: [
-                         const BoxShadow(
-                             color: Color(0x4c000000),
-                             offset: Offset.zero,
-                             blurRadius: Dimens.px20,
-                             spreadRadius: 0)
-                       ],
-                     ),
-                     child: Column(
-                       children: [
-                         Container(
-                             width: scaleWidth(Dimens.px37),
-                             height: Dimens.px5,
-                             margin: const EdgeInsets.symmetric(
-                                 vertical: Dimens.px16),
-                             decoration: BoxDecoration(
-                                 color: ColorsRes.textFieldBorder,
-                                 borderRadius:
-                                 BorderRadius.circular(Dimens.px3))),
-                         Expanded(
-                           child: Padding(
-                             padding: const EdgeInsets.all(
-                                 Dimens.px16),
-                             child: Row(
-                               children: [
-
-                               ],
-                             ),
-                           ),
-                         )
-                       ],
-                     ),
-                   );
-                 },
-               ),
-             ),
-           );
-         },
-         barrierColor: ColorsRes.dialogDimBg);
-   }
-
+  showImagePickerDialog() {
+    showModalBottomSheet(
+        context: context,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(Dimens.px5),
+                topRight: Radius.circular(Dimens.px5))),
+        enableDrag: true,
+        backgroundColor: Colors.transparent,
+        isScrollControlled: true,
+        builder: (context) {
+          final width = (screenSize.width - 4 * Dimens.px16) / 3;
+          return GestureDetector(
+            onTap: () {
+              pop(PageIntent(context, null));
+            },
+            child: Container(
+              color: Colors.transparent,
+              child: DraggableScrollableSheet(
+                maxChildSize: 1,
+                expand: true,
+                builder: (c, scrollController) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(Dimens.px5),
+                        topRight: Radius.circular(Dimens.px5),
+                      ),
+                      boxShadow: [
+                        const BoxShadow(
+                            color: Color(0x4c000000),
+                            offset: Offset.zero,
+                            blurRadius: Dimens.px20,
+                            spreadRadius: 0)
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                            width: scaleWidth(Dimens.px37),
+                            height: Dimens.px5,
+                            margin: const EdgeInsets.symmetric(
+                                vertical: Dimens.px16),
+                            decoration: BoxDecoration(
+                                color: ColorsRes.textFieldBorder,
+                                borderRadius:
+                                    BorderRadius.circular(Dimens.px3))),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(Dimens.px16),
+                            child: Row(
+                              children: [],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          );
+        },
+        barrierColor: ColorsRes.dialogDimBg);
+  }
 }
