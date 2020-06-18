f7804985ecc6380c65cfe94edce76c911726c017
tungpham <phamthanhtung1995@gmail.com>
2020-06-18 21:10:43 +0700

    add kaylee_state.dart: showloading indicator

---
+++ lib/base/kaylee_state.dart
@@ -0,0 +1,35 @@
+import 'package:anth_package/anth_package.dart' hide VoidCallback;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:kaylee/res/res.dart';
+
+abstract class KayleeState<T extends StatefulWidget> extends BaseState<T> {
+  bool isShowLoading = false;
+
+  void showLoading({bool canDismiss = false, VoidCallback onDismiss}) {
+    if (!isShowLoading) {
+      isShowLoading = true;
+      showGeneralDialog(
+        context: context,
+        barrierDismissible: canDismiss,
+        pageBuilder: (BuildContext context, Animation<double> animation,
+            Animation<double> secondaryAnimation) {
+          return CupertinoActivityIndicator(
+            radius: Dimens.px16,
+          );
+        },
+        transitionDuration: Duration(milliseconds: 200),
+        barrierColor: ColorsRes.shadow,
+      ).then((value) {
+        isShowLoading = false;
+        if (onDismiss.isNotNull) onDismiss();
+      });
+    }
+  }
+
+  void hideLoading() {
+    if (isShowLoading) {
+      popScreen();
+    }
+  }
+}
