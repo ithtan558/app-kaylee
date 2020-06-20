9c21883e5b4450defba0e5b9e6166d74ee3398ed
tungpham <phamthanhtung1995@gmail.com>
2020-06-20 23:44:24 +0700

    load policy from api

--- lib/widgets/src/policy_checkbox.dart
+++ lib/widgets/src/policy_checkbox.dart
@@ -2,10 +2,13 @@ import 'package:anth_package/anth_package.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
+import 'package:kaylee/base/networks/network_module.dart';
+import 'package:kaylee/models/models.dart';
 import 'package:kaylee/res/res.dart';
 import 'package:kaylee/res/src/dimens.dart';
 import 'package:kaylee/res/src/images.dart';
-import 'package:kaylee/res/src/strings.dart';
+import 'package:kaylee/services/services.dart';
 import 'package:kaylee/widgets/kaylee_widgets.dart';
 
 class PolicyCheckBox extends StatefulWidget {
@@ -49,30 +52,8 @@ class _PolicyCheckBoxState extends BaseState<PolicyCheckBox> {
                       showKayleeBottomSheet(context,
                           maxChildSize: 635 / 667, initialChildSize: 635 / 667,
                           builder: (context, scrollController) {
-                        return Column(
-                          children: [
-                            KayleeText.normal16W500(
-                              Strings.dieuKhoanVaDieuKien.toUpperCase(),
-                              overflow: TextOverflow.visible,
-                              maxLines: 1,
-                            ),
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.only(top: Dimens.px16),
-                                child: SingleChildScrollView(
-                                  controller: scrollController,
-                                  padding: const EdgeInsets.only(
-                                      left: Dimens.px16,
-                                      right: Dimens.px16,
-                                      bottom: Dimens.px16),
-                                  child: KayleeText.normal16W400(
-                                    Strings.policyContent,
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
+                        return _PolicyView.newInstance(
+                          scrollController: scrollController,
                         );
                       });
                     },
@@ -85,3 +66,140 @@ class _PolicyCheckBoxState extends BaseState<PolicyCheckBox> {
     );
   }
 }
+
+class _PolicyView extends StatefulWidget {
+  static Widget newInstance({ScrollController scrollController}) =>
+      BlocProvider(
+        create: (context) => _PolicyViewBloc(
+            RepositoryProvider.of<NetworkModule>(context)
+                .provideCommonService()),
+        child: _PolicyView._(
+          scrollController: scrollController,
+        ),
+      );
+
+  final ScrollController scrollController;
+
+  _PolicyView._({this.scrollController});
+
+  @override
+  _PolicyViewState createState() => _PolicyViewState();
+}
+
+class _PolicyViewState extends BaseState<_PolicyView> {
+  _PolicyViewBloc bloc;
+
+  @override
+  void initState() {
+    super.initState();
+    bloc = BlocProvider.of<_PolicyViewBloc>(context);
+    bloc.loadPolicy();
+  }
+
+  @override
+  void dispose() {
+    bloc.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<_PolicyViewBloc, dynamic>(
+      builder: (context, state) {
+        if (state is LoadingState) {
+          return CupertinoActivityIndicator();
+        } else if (state is ErrorState) {
+          return Container();
+        } else if (state is _SuccessPolicyViewState) {
+          return Column(
+            children: [
+              KayleeText.normal16W500(
+                state.content?.name,
+                overflow: TextOverflow.visible,
+                maxLines: 1,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: Dimens.px16),
+                  child: SingleChildScrollView(
+                    controller: widget.scrollController,
+                    padding: const EdgeInsets.only(
+                        left: Dimens.px16,
+                        right: Dimens.px16,
+                        bottom: Dimens.px16),
+                    child: HtmlWidget(
+                      state.content.content,
+                      textStyle: TextStyles.normal16W400,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          );
+        } else
+          return Container();
+      },
+      listener: (context, state) {
+        if (state is ErrorState) {
+          showKayleeAlertDialog(
+            context: context,
+            view: KayleeAlertDialogView.error(
+              error: state.error,
+              actions: [
+                KayleeAlertDialogAction.huy(
+                  onPressed: () {
+                    popScreen();
+                  },
+                ),
+              ],
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+class _PolicyViewBloc extends BaseBloc {
+  CommonService commonService;
+
+  _PolicyViewBloc(this.commonService);
+
+  @override
+  Stream mapEventToState(e) async* {
+    if (e is ErrorEvent) {
+      yield* errorState(e);
+    } else if (e is _SuccessPolicyViewEvent) {
+      yield _SuccessPolicyViewState(e.content);
+    } else if (e is _LoadPolicyViewEvent) {
+      yield LoadingState();
+      RequestHandler(
+        request: commonService.getContent(Content.POLICY_HASHTAG),
+        onSuccess: ({message, result}) {
+          add(_SuccessPolicyViewEvent(result));
+        },
+        onFailed: (code, {error}) {
+          errorEvent(code, error: error);
+        },
+      );
+    }
+  }
+
+  void loadPolicy() {
+    add(_LoadPolicyViewEvent());
+  }
+}
+
+class _SuccessPolicyViewEvent {
+  Content content;
+
+  _SuccessPolicyViewEvent(this.content);
+}
+
+class _SuccessPolicyViewState {
+  Content content;
+
+  _SuccessPolicyViewState(this.content);
+}
+
+class _LoadPolicyViewEvent {}
