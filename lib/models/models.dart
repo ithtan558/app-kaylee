052e29f57a46474319c3f6e3e4d26852f6aead2c
tungpham <phamthanhtung1995@gmail.com>
2020-06-20 12:45:46 +0700

    add policy.dart

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -6,5 +6,6 @@ export 'src/request/verify_otp/verify_otp_body.dart';
 export 'src/request/verify_phone/verify_phone_body.dart';
 export 'src/response/error_code.dart';
 export 'src/response/login/login_result.dart';
+export 'src/response/policy/policy.dart';
 export 'src/response/user_info/user_info.dart';
 export 'src/response/verify_phone/verify_phone_result.dart';
