874ee2476bb7058d9760e7f2b26537a5cb2ec260
tungpham <phamthanhtung1995@gmail.com>
2020-06-20 14:31:34 +0700

    add user_module.dart

---
+++ lib/base/user/user_module.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+
+import 'package:anth_package/anth_package.dart';
+import 'package:flutter/foundation.dart';
+import 'package:kaylee/models/models.dart';
+
+const _SHARE_REF_AUTHENTICATE = 'SHARE_REF_LOGIN_INFO';
+
+class _UserModuleImpl extends UserModule {
+  LoginResult loginResult;
+
+  _UserModuleImpl._() : super._() {
+    forceGetUserInfo();
+  }
+
+  @override
+  LoginResult getUserInfo() {
+    return loginResult;
+  }
+
+  @override
+  void updateUserInfo(LoginResult auth) {
+    loginResult = auth;
+    final json = jsonEncode(auth);
+    SharedRef.putString(_SHARE_REF_AUTHENTICATE, json);
+  }
+
+  @override
+  void removeUserInfo() {
+    loginResult = LoginResult();
+    SharedRef.putString(_SHARE_REF_AUTHENTICATE, jsonEncode({}));
+  }
+
+  @override
+  void forceGetUserInfo({ValueSetter<LoginResult> callback}) {
+    final json = SharedRef.getString(_SHARE_REF_AUTHENTICATE);
+    loginResult = LoginResult.fromJson(jsonDecode(json ?? '{}'));
+    if (callback != null) {
+      callback(loginResult);
+    }
+  }
+}
+
+abstract class UserModule {
+  factory UserModule.init() => _UserModuleImpl._();
+
+  UserModule._();
+
+  LoginResult getUserInfo();
+
+  void forceGetUserInfo({ValueSetter<LoginResult> callback});
+
+  void updateUserInfo(LoginResult auth);
+
+  void removeUserInfo();
+}
