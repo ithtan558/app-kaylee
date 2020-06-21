5b3888ea69efb970a3e891f62a87b4b16dac191b
tungpham <phamthanhtung1995@gmail.com>
2020-06-22 00:36:04 +0700

    remove request model

--- lib/base/json_converter/kaylee_json_convert.dart
+++ lib/base/json_converter/kaylee_json_convert.dart
@@ -6,20 +6,10 @@ class KayleeJsonConverter extends JsonConverterFactory {
   T fromJson<T>(json) {
     if (T == LoginResult) {
       return LoginResult.fromJson(json) as T;
-    } else if (T == RegisterBody) {
-      return RegisterBody.fromJson(json) as T;
     } else if (T == UserInfo) {
       return UserInfo.fromJson(json) as T;
     } else if (T == VerifyPhoneResult) {
       return VerifyPhoneResult.fromJson(json) as T;
-    } else if (T == VerifyOtpBody) {
-      return VerifyOtpBody.fromJson(json) as T;
-    } else if (T == VerifyPhoneBody) {
-      return VerifyPhoneBody.fromJson(json) as T;
-    } else if (T == LoginBody) {
-      return LoginBody.fromJson(json) as T;
-    } else if (T == LoginBody) {
-      return LoginBody.fromJson(json) as T;
     } else if (T == Content) {
       return Content.fromJson(json) as T;
     } else if (T == VerifyOtpResult) {
@@ -32,17 +22,9 @@ class KayleeJsonConverter extends JsonConverterFactory {
   toJson<T>(T json) {
     if (json is LoginResult) {
       return json.toJson();
-    } else if (json is RegisterBody) {
-      return json.toJson();
     } else if (json is UserInfo) {
       return json.toJson();
     } else if (json is VerifyPhoneResult) {
-      return json.toJson();
-    } else if (json is VerifyOtpBody) {
-      return json.toJson();
-    } else if (json is VerifyPhoneBody) {
-      return json.toJson();
-    } else if (json is LoginBody) {
       return json.toJson();
     } else if (json is Content) {
       return json.toJson();
