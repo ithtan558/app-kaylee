bce25034a32a50cf482139aec462de3831c32278
tungpham <phamthanhtung1995@gmail.com>
2020-07-14 22:12:54 +0700

    show name user_name.dart

--- lib/screens/src/home/tabs/home/widgets/home_menu/user_name.dart
+++ lib/screens/src/home/tabs/home/widgets/home_menu/user_name.dart
@@ -1,5 +1,7 @@
 import 'package:anth_package/anth_package.dart';
 import 'package:flutter/material.dart';
+import 'package:kaylee/base/user/user_module.dart';
+import 'package:kaylee/models/models.dart';
 import 'package:kaylee/res/res.dart';
 import 'package:kaylee/screens/src/home/tabs/home/widgets/home_menu/home_menu.dart';
 import 'package:kaylee/widgets/widgets.dart';
@@ -13,6 +15,7 @@ class _UserNameState extends BaseState<UserName> {
   final positionController = BehaviorSubject<double>();
   final opacityController = BehaviorSubject<double>();
   final marginTop = Dimens.px56;
+  UserInfo userinfo;
 
   @override
   void initState() {
@@ -28,6 +31,8 @@ class _UserNameState extends BaseState<UserName> {
       opacityController
           .add(1 - (marginTop - (namePosition ?? marginTop)) / Dimens.px16);
     });
+
+    userinfo = context.repository<UserModule>().getUserInfo()?.userInfo;
   }
 
   @override
@@ -39,7 +44,7 @@ class _UserNameState extends BaseState<UserName> {
 
   @override
   Widget build(BuildContext context) {
-    final userName = KayleeText.normalWhite16W500("Hi, Huynh An");
+    final userName = KayleeText.normalWhite16W500("Hi, ${userinfo?.firstName}");
     final role = KayleeText.normalWhite12W400(
       "Quản lý cửa hàng",
     );
