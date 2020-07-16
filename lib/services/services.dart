04c7dd84c4eec56091b30b7eb258ab41f69b9f25
tungpham <phamthanhtung1995@gmail.com>
2020-07-16 21:31:25 +0700

    export notification_service.dart

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,6 +1,7 @@
 library services;
 
 export 'src/common/common_service.dart';
+export 'src/notification/notification_service.dart';
 export 'src/product/product_service.dart';
 export 'src/supplier/supplier_service.dart';
 export 'src/user/user_service.dart';
