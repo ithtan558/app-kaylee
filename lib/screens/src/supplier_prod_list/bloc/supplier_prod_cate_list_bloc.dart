3ab9b4584c123b28c0cab4f253c5ea4b1ba6df54
tung <tungpt@sendo.vn>
2020-07-17 15:44:44 +0700

    refactor SuppProCateState

--- lib/screens/src/supplier_prod_list/bloc/supplier_prod_cate_list_bloc.dart
+++ lib/screens/src/supplier_prod_list/bloc/supplier_prod_cate_list_bloc.dart
@@ -4,28 +4,45 @@ import 'package:flutter/cupertino.dart';
 import 'package:kaylee/models/models.dart';
 import 'package:kaylee/services/services.dart';
 
-class SupplierProdCateListBloc extends Cubit {
+class SupplierProdCateListBloc extends Cubit<SuppProCateState> {
   ProductService productService;
 
   SupplierProdCateListBloc({@required this.productService})
-      : super(SuppProCateState(categories: []));
+      : super(SuppProCateState(categories: [])..loading = true);
 
   void loadProdCate({@required int supplierId}) {
-    emit(LoadingDialogState());
+    emit(SuppProCateState.copy(state..loading = true));
     RequestHandler(
       request: productService.getProdCategory(supplier_id: supplierId),
       onSuccess: ({message, result}) {
-        emit(SuppProCateState(categories: result));
+        emit(SuppProCateState.copy(state
+          ..categories = result
+          ..loading = false
+          ..code = null
+          ..error = null));
       },
       onFailed: (code, {error}) {
-        emit(ErrorState(code, error: error));
+        emit(SuppProCateState.copy(state
+          ..loading = false
+          ..code = code
+          ..error = error));
       },
     );
   }
 }
 
-class SuppProCateState {
+class SuppProCateState extends BaseModelState {
   List<ProdCate> categories;
 
-  SuppProCateState({this.categories});
+  SuppProCateState({
+    this.categories,
+  });
+
+  SuppProCateState.copy(SuppProCateState old) {
+    this
+      ..categories = old?.categories
+      ..loading = old?.loading
+      ..error = old?.error
+      ..code = old?.code;
+  }
 }
