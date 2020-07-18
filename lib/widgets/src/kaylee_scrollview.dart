22f435b5fdac72cc6ad626feb645ab610a33da27
tungpham <phamthanhtung1995@gmail.com>
2020-07-18 23:48:46 +0700

    add bottom kaylee_scrollview.dart

--- lib/widgets/src/kaylee_scrollview.dart
+++ lib/widgets/src/kaylee_scrollview.dart
@@ -6,8 +6,9 @@ class KayleeScrollview extends StatefulWidget {
   final KayleeAppBar appBar;
   final Widget child;
   final EdgeInsets padding;
+  final Widget bottom;
 
-  KayleeScrollview({this.appBar, this.child, this.padding});
+  KayleeScrollview({this.appBar, this.child, this.padding, this.bottom});
 
   @override
   _KayleeScrollviewState createState() => _KayleeScrollviewState();
@@ -18,9 +19,16 @@ class _KayleeScrollviewState extends State<KayleeScrollview> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: widget.appBar,
-      body: SingleChildScrollView(
-        padding: widget.padding ?? EdgeInsets.zero,
-        child: widget.child,
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              padding: widget.padding ?? EdgeInsets.zero,
+              child: widget.child,
+            ),
+          ),
+          widget.bottom,
+        ],
       ),
     );
   }
