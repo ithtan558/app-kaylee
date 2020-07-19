6c5128237185f37585502be207636d1fd8442630
tungpham <phamthanhtung1995@gmail.com>
2020-07-19 14:10:04 +0700

    update cart_screen.dart

--- lib/screens/src/cart/cart_screen.dart
+++ lib/screens/src/cart/cart_screen.dart
@@ -1,8 +1,11 @@
 import 'package:core_plugin/core_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:kaylee/app_bloc.dart';
+import 'package:kaylee/models/models.dart';
 import 'package:kaylee/res/res.dart';
 import 'package:kaylee/screens/screens.dart';
 import 'package:kaylee/screens/src/cart/widgets/cart_prod_item.dart';
+import 'package:kaylee/utils/utils.dart';
 import 'package:kaylee/widgets/widgets.dart';
 
 class CartScreen extends StatefulWidget {
@@ -25,8 +28,6 @@ class _CartScreenState extends BaseState<CartScreen> {
     super.dispose();
   }
 
-  final list = [for (int i = 0; i <= 3; i++) i];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,24 +37,39 @@ class _CartScreenState extends BaseState<CartScreen> {
       body: Column(
         children: [
           Expanded(
-            child: ListView.separated(
-                itemBuilder: (c, index) {
-                  if (index < list.length)
-                    return CartProdItem(
-                      onRemoveItem: () {
-                        setState(() {
-                          list.removeWhere((e) => e == list.elementAt(index));
-                        });
-                      },
-                    );
-                  return buildTotalAmountInfo();
-                },
-                separatorBuilder: (_, index) => Container(
-                    height: 1,
-                    margin: const EdgeInsets.symmetric(horizontal: Dimens.px16),
-                    decoration:
-                        new BoxDecoration(color: ColorsRes.textFieldBorder)),
-                itemCount: (list?.length ?? 0) + 1),
+            child: CubitBuilder<CartBloc, CartState>(
+              builder: (context, state) {
+                final cartItems = context.cart.getOrder()?.cartItems;
+                if (cartItems.isNullOrEmpty) {
+                  return Container(
+                    margin: EdgeInsets.only(top: Dimens.px16),
+                    child: KayleeText.hint16W400(Strings.banChuaChonSanPham),
+                  );
+                }
+                return ListView.separated(
+                    itemBuilder: (c, index) {
+                      if (index < cartItems.length) {
+                        final item = cartItems?.elementAt(index);
+                        return CartProdItem(
+                          product: item,
+                          onRemoveItem: () {
+                            context.cart.removeProd(item);
+                            context.cubit<CartBloc>().updateCart();
+                          },
+                        );
+                      }
+                      return buildTotalAmountInfo(cartItems);
+                    },
+                    separatorBuilder: (_, index) => Container(
+                          height: 1,
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: Dimens.px16),
+                          decoration: new BoxDecoration(
+                              color: ColorsRes.textFieldBorder),
+                        ),
+                    itemCount: (cartItems?.length ?? 0) + 1);
+              },
+            ),
           ),
           KayLeeRoundedButton.normal(
             text: Strings.datHang,
@@ -67,20 +83,27 @@ class _CartScreenState extends BaseState<CartScreen> {
     );
   }
 
-  Widget buildTotalAmountInfo() {
+  Widget buildTotalAmountInfo(List cartItems) {
+    final total = cartItems.fold(0, (previousValue, e) {
+      int price = 0;
+      if (e is Product) {
+        price = previousValue + e.price * e.quantity;
+      }
+      return price;
+    });
     return Padding(
       padding: const EdgeInsets.all(Dimens.px16),
       child: Column(
         children: [
           KayleeTitlePriceText.normal(
             title: Strings.tongChiPhi,
-            price: 5950000,
+            price: total,
           ),
           Padding(
             padding: const EdgeInsets.only(top: Dimens.px8),
             child: KayleeTitlePriceText.bold(
-              title: 'Thành tiền',
-              price: 5950000,
+              title: Strings.thanhTien,
+              price: total,
             ),
           ),
         ],
