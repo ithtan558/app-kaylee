8e3bfde0a3009ef0cacc2baf623048a3dcf62ee4
tungpham <phamthanhtung1995@gmail.com>
2020-07-19 15:33:57 +0700

    add api getCity, getDistrict, getWard

--- lib/services/src/common/common_service.dart
+++ lib/services/src/common/common_service.dart
@@ -10,4 +10,13 @@ abstract class CommonService {
 
   @GET('content/{hashtag}')
   Future<ResponseModel<Content>> getContent(@Path() String hashtag);
+
+  @GET('city/all')
+  Future<ResponseModel<City>> getCity();
+
+  @GET('district/list-by-city/{city}')
+  Future<ResponseModel<District>> getDistrict(@Path() int city);
+
+  @GET('wards/list-by-district/{district}')
+  Future<ResponseModel<Ward>> getWard(@Path() int district);
 }
--- lib/services/src/common/common_service.g.dart
+++ lib/services/src/common/common_service.g.dart
@@ -33,4 +33,60 @@ class _CommonService implements CommonService {
     final value = ResponseModel<Content>.fromJson(_result.data);
     return value;
   }
+
+  @override
+  getCity() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'city/all',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<City>.fromJson(_result.data);
+    return value;
+  }
+
+  @override
+  getDistrict(city) async {
+    ArgumentError.checkNotNull(city, 'city');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'district/list-by-city/$city',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<District>.fromJson(_result.data);
+    return value;
+  }
+
+  @override
+  getWard(district) async {
+    ArgumentError.checkNotNull(district, 'district');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'wards/list-by-district/$district',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<Ward>.fromJson(_result.data);
+    return value;
+  }
 }
