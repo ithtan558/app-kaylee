05fda92f813c1e4e003f0f61f72458568315ebcc
tungpham <phamthanhtung1995@gmail.com>
2020-07-19 11:21:37 +0700

    add add service.dart

---
+++ lib/models/src/response/service/list/services.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'services.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Services _$ServicesFromJson(Map<String, dynamic> json) {
+  return Services()
+    ..page = json['page'] as int
+    ..limit = json['limit'] as int
+    ..total = json['total'] as int
+    ..pages = json['pages'] as int
+    ..items = json['items'] == null
+        ? null
+        : Service.fromJson(json['items'] as Map<String, dynamic>);
+}
+
+Map<String, dynamic> _$ServicesToJson(Services instance) => <String, dynamic>{
+      'page': instance.page,
+      'limit': instance.limit,
+      'total': instance.total,
+      'pages': instance.pages,
+      'items': instance.items,
+    };
