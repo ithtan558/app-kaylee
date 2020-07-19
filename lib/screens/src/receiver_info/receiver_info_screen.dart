c4ff7d343f7a3fea6335aba7cece42841982be56
tungpham <phamthanhtung1995@gmail.com>
2020-07-19 14:31:52 +0700

    re-layout

--- lib/screens/src/receiver_info/receiver_info_screen.dart
+++ lib/screens/src/receiver_info/receiver_info_screen.dart
@@ -26,51 +26,49 @@ class _ReceiverInfoScreenState extends BaseState<ReceiverInfoScreen> {
   @override
   Widget build(BuildContext context) {
     return UnFocusWidget(
-      child: Scaffold(
+      child: KayleeScrollview(
         appBar: KayleeAppBar(
           title: Strings.thongTinNguoiNhan,
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: Dimens.px16, right: Dimens.px16, left: Dimens.px16),
-                child: KayleeTextField.normal(
-                  title: Strings.hoVaTen,
-                  hint: Strings.nhapHoVaTen,
-                ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: Dimens.px16, right: Dimens.px16, left: Dimens.px16),
+              child: KayleeTextField.normal(
+                title: Strings.hoVaTen,
+                hint: Strings.nhapHoVaTen,
               ),
-              KayleeFullAddressInput(
-                title: Strings.diaChiHienTai,
-                padding: EdgeInsets.all(Dimens.px16),
+            ),
+            KayleeFullAddressInput(
+              title: Strings.diaChiHienTai,
+              padding: EdgeInsets.all(Dimens.px16),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: Dimens.px16, right: Dimens.px16, bottom: Dimens.px16),
+              child: KayleeTextField.phoneInput(
+                title: Strings.soDienThoai,
               ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: Dimens.px16, right: Dimens.px16, bottom: Dimens.px16),
-                child: KayleeTextField.phoneInput(
-                  title: Strings.soDienThoai,
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: Dimens.px16, right: Dimens.px16, bottom: Dimens.px16),
+              child: KayleeTextField.multiLine(
+                title: Strings.luuY,
+                hint: Strings.luuYHint,
+                textInputAction: TextInputAction.newline,
+                fieldHeight:
+                    (context.screenSize.width - Dimens.px32) / (343 / 233),
+                contentPadding: EdgeInsets.symmetric(vertical: Dimens.px16),
               ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: Dimens.px16, right: Dimens.px16, bottom: Dimens.px16),
-                child: KayleeTextField.multiLine(
-                  title: Strings.luuY,
-                  hint: Strings.luuYHint,
-                  textInputAction: TextInputAction.newline,
-                  fieldHeight:
-                      (context.screenSize.width - Dimens.px32) / (343 / 233),
-                  contentPadding: EdgeInsets.symmetric(vertical: Dimens.px16),
-                ),
-              ),
-              KayLeeRoundedButton.normal(
-                text: Strings.tiepTuc,
-                margin: EdgeInsets.all(Dimens.px8),
-                onPressed: () {},
-              )
-            ],
-          ),
+            ),
+          ],
+        ),
+        bottom: KayLeeRoundedButton.normal(
+          text: Strings.tiepTuc,
+          margin: EdgeInsets.all(Dimens.px8),
+          onPressed: () {},
         ),
       ),
     );
