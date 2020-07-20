548ef963f942e271bf83ab865f0260ef67638b08
tungpham <phamthanhtung1995@gmail.com>
2020-07-21 01:15:42 +0700

    add serv_service.dart

---
+++ lib/services/src/service/serv_service.dart
@@ -0,0 +1,21 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:dio/dio.dart';
+import 'package:kaylee/models/models.dart';
+
+part 'serv_service.g.dart';
+
+@RestApi()
+abstract class ServService {
+  factory ServService(Dio dio) = _ServService;
+
+  @GET('service-category/all')
+  Future<ResponseModel<Category>> getCategory();
+
+  @GET('service')
+  Future<ResponseModel<Services>> getServices(
+      {@Query('keyword') String keyword,
+      @Query('page') int page,
+      @Query('limit') int limit,
+      @Query('sort') String sort,
+      @Query('category_id') int categoryId});
+}
---
+++ lib/services/src/service/serv_service.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'serv_service.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+class _ServService implements ServService {
+  _ServService(this._dio, {this.baseUrl}) {
+    ArgumentError.checkNotNull(_dio, '_dio');
+  }
+
+  final Dio _dio;
+
+  String baseUrl;
+
+  @override
+  getCategory() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'service-category/all',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<Category>.fromJson(_result.data);
+    return value;
+  }
+
+  @override
+  getServices({keyword, page, limit, sort, categoryId}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      r'keyword': keyword,
+      r'page': page,
+      r'limit': limit,
+      r'sort': sort,
+      r'category_id': categoryId
+    };
+    queryParameters.removeWhere((k, v) => v == null);
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request('service',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<Services>.fromJson(_result.data);
+    return value;
+  }
+}
