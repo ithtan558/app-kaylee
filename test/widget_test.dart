35c960d909c3eec0f34cab8338b438ea1c8d24a9
tungpham <phamthanhtung1995@gmail.com>
2020-07-23 00:47:12 +0700

    nothing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:anth_package/anth_package.dart';
+
 void main() {
   print('[TUNG] ===> ${(1370 / 343).round()}');
+  final dateTime =
+      DateFormat('${'h' * 2}:${'m' * 2}:${'s' * 2}').parse('08:00:00');
+
+  dateTime.hour;
 }
