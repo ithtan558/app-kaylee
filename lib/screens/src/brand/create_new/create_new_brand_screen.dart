216ffc52791b575b0b8feac45026b5ee47817ca4
tungpham <phamthanhtung1995@gmail.com>
2020-07-29 01:07:45 +0700

    update create_new_brand_screen.dart

--- lib/screens/src/brand/create_new/create_new_brand_screen.dart
+++ lib/screens/src/brand/create_new/create_new_brand_screen.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
+
 import 'package:anth_package/anth_package.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kaylee/base/kaylee_state.dart';
 import 'package:kaylee/models/models.dart';
 import 'package:kaylee/res/res.dart';
+import 'package:kaylee/screens/src/brand/create_new/bloc/brand_detail_screen_bloc.dart';
+import 'package:kaylee/utils/utils.dart';
 import 'package:kaylee/widgets/src/kaylee_picker_textfield.dart';
 import 'package:kaylee/widgets/widgets.dart';
 
@@ -16,7 +21,12 @@ class NewBrandScreenData {
 enum BrandScreenOpenFrom { brandItem, addNewBrandBtn }
 
 class CreateNewBrandScreen extends StatefulWidget {
-  static Widget newInstance() => CreateNewBrandScreen._();
+  static Widget newInstance() => BlocProvider<BrandDetailScreenBloc>(
+        create: (context) => BrandDetailScreenBloc(
+            brandService: context.network.provideBrandService(),
+            brand: context.getArguments<NewBrandScreenData>().brand),
+        child: CreateNewBrandScreen._(),
+      );
 
   CreateNewBrandScreen._();
 
@@ -24,7 +34,7 @@ class CreateNewBrandScreen extends StatefulWidget {
   _CreateNewBrandScreenState createState() => _CreateNewBrandScreenState();
 }
 
-class _CreateNewBrandScreenState extends BaseState<CreateNewBrandScreen> {
+class _CreateNewBrandScreenState extends KayleeState<CreateNewBrandScreen> {
   BrandScreenOpenFrom openFrom;
   final startTimeController = PickInputController<StartTime>();
   final endTimeController = PickInputController<EndTime>();
@@ -34,28 +44,46 @@ class _CreateNewBrandScreenState extends BaseState<CreateNewBrandScreen> {
   final phoneTfController = TextEditingController();
   final phoneFocus = FocusNode();
   String banner;
+  BrandDetailScreenBloc bloc;
+  StreamSubscription brandDetailScreenBlocSub;
+  final bannerPickerController = ImagePickerController();
 
   @override
   void initState() {
     super.initState();
+    bloc = context.bloc<BrandDetailScreenBloc>();
+    brandDetailScreenBlocSub = bloc.listen((state) {
+      if (state.loading) {
+        showLoading();
+      } else if (!state.loading) {
+        hideLoading();
+        if (state.code.isNotNull && state.code != ErrorType.UNAUTHORIZED) {
+          showKayleeAlertErrorYesDialog(
+            context: context,
+            error: state.error,
+            onPressed: () {
+              popScreen();
+            },
+          );
+        } else if (state.message.isNotNull) {
+          showKayleeAlertMessageYesDialog(
+              context: context,
+              message: state.message,
+              onPressed: popScreen,
+              onDismiss: popScreen);
+        }
+      }
+    });
     final data = context.getArguments<NewBrandScreenData>();
     openFrom = data?.openFrom;
-    if (openFrom == BrandScreenOpenFrom.brandItem && data.brand.isNotNull) {
-      banner = data.brand.image;
-      nameTfController.text = data.brand.name ?? '';
-      addressController
-        ..initAddress = data.brand.location ?? ''
-        ..initCity = data.brand.city
-        ..initDistrict = data.brand.district
-        ..initWard = data.brand.wards;
-      phoneTfController.text = data.brand.phone ?? '';
-      startTimeController.value = data.brand.start;
-      endTimeController.value = data.brand.end;
+    if (openFrom == BrandScreenOpenFrom.brandItem) {
+      bloc.get();
     }
   }
 
   @override
   void dispose() {
+    brandDetailScreenBlocSub.cancel();
     nameTfController.dispose();
     phoneTfController.dispose();
     nameFocus.dispose();
@@ -79,76 +107,160 @@ class _CreateNewBrandScreenState extends BaseState<CreateNewBrandScreen> {
                 text: openFrom == BrandScreenOpenFrom.brandItem
                     ? Strings.luu
                     : Strings.tao,
-                onTap: () {},
+                onTap: () {
+                  if (openFrom == BrandScreenOpenFrom.brandItem) {
+                    showKayleeAlertDialog(
+                        context: context,
+                        view: KayleeAlertDialogView(
+                          title: Strings.banDaChacChan,
+                          content: Strings.banCoDongYLuuLaiNhungThayDoi,
+                          actions: [
+                            KayleeAlertDialogAction.dongY(
+                              onPressed: () {
+                                popScreen();
+                                bloc.update(bloc.state.item
+                                  ..name = nameTfController.text
+                                  ..phone = phoneTfController.text
+                                  ..location = addressController.address
+                                  ..city = addressController.city
+                                  ..district = addressController.district
+                                  ..startTime = startTimeController.value.time
+                                  ..endTime = endTimeController.value.time
+                                  ..wards = addressController.ward
+                                  ..imageFile = bannerPickerController.image);
+                              },
+                              isDefaultAction: true,
+                            ),
+                            KayleeAlertDialogAction.huy(
+                              onPressed: popScreen,
+                            ),
+                          ],
+                        ));
+                  } else {
+                    bloc.create(Brand(
+                        name: nameTfController.text,
+                        phone: phoneTfController.text,
+                        location: addressController.address,
+                        city: addressController.city,
+                        district: addressController.district,
+                        startTime: startTimeController.value.time,
+                        endTime: endTimeController.value.time,
+                        wards: addressController.ward,
+                        imageFile: bannerPickerController.image));
+                  }
+                },
               ),
             )
           ],
         ),
-        child: Column(
-          children: [
-            KayleeImagePicker(
-              type: KayleeImagePickerType.banner,
-              image: banner,
-              onImageSelect: (file, {existedImage}) {},
-            ),
-            Padding(
-              padding: const EdgeInsets.all(Dimens.px16),
-              child: KayleeTextField.normal(
-                title: Strings.tenCuaHang,
-                hint: Strings.tenCuaHangHint,
-                controller: nameTfController,
-                focusNode: nameFocus,
-                textInputAction: TextInputAction.next,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: Dimens.px16, right: Dimens.px16, bottom: Dimens.px16),
-              child: KayleeFullAddressInput(
-                title: Strings.diaChi,
-                controller: addressController,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: Dimens.px16, right: Dimens.px16, bottom: Dimens.px16),
-              child: KayleeTextField.phoneInput(
-                title: Strings.soDienThoai,
-                textInputAction: TextInputAction.done,
-                focusNode: phoneFocus,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: Dimens.px16, right: Dimens.px16, bottom: Dimens.px16),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: KayleePickerTextField<StartTime>(
-                      title: Strings.gioMoCua,
-                      controller: startTimeController,
-                    ),
+        child: BlocBuilder<BrandDetailScreenBloc, SingleModel<Brand>>(
+          buildWhen: (previous, current) => !current.loading,
+          builder: (context, state) {
+            print('[TUNG] ===> state.loading ${state.loading}');
+            banner = state.item?.image;
+            nameTfController.text = state.item?.name ?? '';
+            addressController
+              ..initAddress = state.item?.location ?? ''
+              ..initCity = state.item?.city
+              ..initDistrict = state.item?.district
+              ..initWard = state.item?.wards;
+            phoneTfController.text = state.item?.phone ?? '';
+            startTimeController.value = state.item?.start;
+            endTimeController.value = state.item?.end;
+            return Column(
+              children: [
+                KayleeImagePicker(
+                  type: KayleeImagePickerType.banner,
+                  image: banner,
+                  controller: bannerPickerController,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(Dimens.px16),
+                  child: KayleeTextField.normal(
+                    title: Strings.tenCuaHang,
+                    hint: Strings.tenCuaHangHint,
+                    controller: nameTfController,
+                    focusNode: nameFocus,
+                    textInputAction: TextInputAction.next,
                   ),
-                  SizedBox(width: Dimens.px8),
-                  Expanded(
-                    child: KayleePickerTextField<EndTime>(
-                      title: Strings.gioDongCua,
-                      controller: endTimeController,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: Dimens.px16,
+                      right: Dimens.px16,
+                      bottom: Dimens.px16),
+                  child: KayleeFullAddressInput(
+                    title: Strings.diaChi,
+                    controller: addressController,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: Dimens.px16,
+                      right: Dimens.px16,
+                      bottom: Dimens.px16),
+                  child: KayleeTextField.phoneInput(
+                    title: Strings.soDienThoai,
+                    textInputAction: TextInputAction.done,
+                    focusNode: phoneFocus,
+                    controller: phoneTfController,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: Dimens.px16,
+                      right: Dimens.px16,
+                      bottom: Dimens.px16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: KayleePickerTextField<StartTime>(
+                          title: Strings.gioMoCua,
+                          controller: startTimeController,
+                        ),
+                      ),
+                      SizedBox(width: Dimens.px8),
+                      Expanded(
+                        child: KayleePickerTextField<EndTime>(
+                          title: Strings.gioDongCua,
+                          controller: endTimeController,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                if (openFrom == BrandScreenOpenFrom.brandItem)
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: Dimens.px16, bottom: Dimens.px32),
+                    child: HyperLinkText(
+                      text: Strings.xoaChiNhanh,
+                      onTap: () {
+                        showKayleeAlertDialog(
+                            context: context,
+                            view: KayleeAlertDialogView(
+                              title: Strings.banDaChacChan,
+                              content: Strings
+                                  .banCoDongYXoaThongTinVaKhongPhucHoiLai,
+                              actions: [
+                                KayleeAlertDialogAction.dongY(
+                                  onPressed: () {
+                                    popScreen();
+                                    bloc.delete();
+                                  },
+                                  isDefaultAction: true,
+                                ),
+                                KayleeAlertDialogAction.huy(
+                                  onPressed: popScreen,
+                                ),
+                              ],
+                            ));
+                      },
                     ),
                   )
-                ],
-              ),
-            ),
-            if (openFrom == BrandScreenOpenFrom.brandItem)
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: Dimens.px16, bottom: Dimens.px32),
-                child: HyperLinkText(
-                  text: Strings.xoaChiNhanh,
-                  onTap: () {},
-                ),
-              )
-          ],
+              ],
+            );
+          },
         ),
       ),
     );
