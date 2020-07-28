0482dd123073b5d085f4bd0c2e54e703b9a85806
tungpham <phamthanhtung1995@gmail.com>
2020-07-29 00:42:52 +0700

    update brand_detail_screen_bloc.dart with CRUDInterface

--- lib/screens/src/brand/create_new/bloc/brand_detail_screen_bloc.dart
+++ lib/screens/src/brand/create_new/bloc/brand_detail_screen_bloc.dart
@@ -1,7 +1,115 @@
-import 'package:cubit/cubit.dart';
+import 'package:anth_package/anth_package.dart';
+import 'package:kaylee/base/crud_interface.dart';
+import 'package:kaylee/models/models.dart';
+import 'package:kaylee/services/services.dart';
 
-class BrandDetailScreenBloc extends Cubit {
-  BrandDetailScreenBloc(initialState) : super(initialState);
+class BrandDetailScreenBloc extends Cubit<SingleModel<Brand>>
+    implements CRUDInterface<Brand> {
+  final BrandService brandService;
 
-  void delete() {}
+  BrandDetailScreenBloc({this.brandService, Brand brand})
+      : super(SingleModel(item: brand));
+
+  @override
+  void create(Brand body) {
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: brandService.newBrand(
+          name: body.name,
+          phone: body.phone,
+          location: body.location,
+          cityId: body.city?.id,
+          districtId: body.district?.id,
+          startTime: body.startTime,
+          endTime: body.endTime,
+          wardsId: body.wards?.id,
+          image: body.imageFile),
+      onSuccess: ({message, result}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..message = message
+          ..error = null
+          ..code = null));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..error = error
+          ..code = code));
+      },
+    );
+  }
+
+  @override
+  void delete() {
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: brandService.deleteBrand(brandId: state.item?.id),
+      onSuccess: ({message, result}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..message = message
+          ..error = null
+          ..code = null));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..error = error
+          ..code = code));
+      },
+    );
+  }
+
+  @override
+  void get() {
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: brandService.getBrand(brandId: state.item?.id),
+      onSuccess: ({message, result}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..item = result
+          ..error = null
+          ..code = null));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..error = error
+          ..code = code));
+      },
+    );
+  }
+
+  @override
+  void update(Brand body) {
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: brandService.updateBrand(
+          name: body.name,
+          phone: body.phone,
+          location: body.location,
+          cityId: body.city?.id,
+          districtId: body.district?.id,
+          startTime: body.startTime,
+          endTime: body.endTime,
+          wardsId: body.wards?.id,
+          image: body.imageFile,
+          brandId: state.item?.id),
+      onSuccess: ({message, result}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..message = message
+          ..error = null
+          ..code = null));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..error = error
+          ..code = code));
+      },
+    );
+  }
 }
