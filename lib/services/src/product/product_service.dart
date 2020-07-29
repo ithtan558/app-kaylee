115f0a12491f45ce59a4ed73b1bd060067a8264c
tungpham <phamthanhtung1995@gmail.com>
2020-07-29 23:42:16 +0700

    update product_service.dart

--- lib/services/src/product/product_service.dart
+++ lib/services/src/product/product_service.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:anth_package/anth_package.dart';
 import 'package:dio/dio.dart';
 import 'package:kaylee/models/models.dart';
@@ -9,10 +11,9 @@ abstract class ProductService {
   factory ProductService(Dio dio) = _ProductService;
 
   @GET('product')
-  Future<ResponseModel<Products>> getProducts(
-      {@Query('supplier_id') int supplierId,
-      @Query('keyword') String keyword,
-      @Query('sort') String sort,
+  Future<ResponseModel<Products>> getProducts({@Query('supplier_id') int supplierId,
+    @Query('keyword') String keyword,
+    @Query('sort') String sort,
       @Query('category_id') int categoryId,
       @Query('page') int page,
       @Query('limit') int limit});
@@ -23,4 +24,33 @@ abstract class ProductService {
 
   @GET('product/{proId}')
   Future<ResponseModel<Product>> getProduct({@Path() int proId});
+
+  @POST('product')
+  @MultiPart()
+  Future<ResponseModel> newProduct({
+    @Part() String name,
+    @Part() String description,
+    @Part(name: 'brand_ids') String brandIds,
+    @Part() int price,
+    @Part() File image,
+    @Part(name: 'category_id') int categoryId,
+  });
+
+  @POST('product/{prodId}')
+  @MultiPart()
+  Future<ResponseModel> updateProduct({
+    @Part() String name,
+    @Part() String description,
+    @Part(name: 'brand_ids') String brandIds,
+    @Part() int price,
+    @Part() File image,
+    @Part(name: 'category_id') int categoryId,
+    @Part() int id,
+    @Path() int prodId,
+  });
+
+  @DELETE('product/delete/{prodId}')
+  Future<ResponseModel> deleteProduct({
+    @Path() int prodId,
+  });
 }
--- lib/services/src/product/product_service.g.dart
+++ lib/services/src/product/product_service.g.dart
@@ -77,4 +77,113 @@ class _ProductService implements ProductService {
     final value = ResponseModel<Product>.fromJson(_result.data);
     return value;
   }
+
+  @override
+  newProduct({name, description, brandIds, price, image, categoryId}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
+    final _data = FormData();
+    if (name != null) {
+      _data.fields.add(MapEntry('name', name));
+    }
+    if (description != null) {
+      _data.fields.add(MapEntry('description', description));
+    }
+    if (brandIds != null) {
+      _data.fields.add(MapEntry('brand_ids', brandIds));
+    }
+    if (price != null) {
+      _data.fields.add(MapEntry('price', price.toString()));
+    }
+    if (image != null) {
+      _data.files.add(MapEntry(
+          'image',
+          MultipartFile.fromFileSync(image.path,
+              filename: image.path.split(Platform.pathSeparator).last)));
+    }
+    if (categoryId != null) {
+      _data.fields.add(MapEntry('category_id', categoryId.toString()));
+    }
+    final Response<Map<String, dynamic>> _result = await _dio.request('product',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'POST',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<dynamic>.fromJson(_result.data);
+    return value;
+  }
+
+  @override
+  updateProduct(
+      {name,
+      description,
+      brandIds,
+      price,
+      image,
+      categoryId,
+      id,
+      prodId}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
+    final _data = FormData();
+    if (name != null) {
+      _data.fields.add(MapEntry('name', name));
+    }
+    if (description != null) {
+      _data.fields.add(MapEntry('description', description));
+    }
+    if (brandIds != null) {
+      _data.fields.add(MapEntry('brand_ids', brandIds));
+    }
+    if (price != null) {
+      _data.fields.add(MapEntry('price', price.toString()));
+    }
+    if (image != null) {
+      _data.files.add(MapEntry(
+          'image',
+          MultipartFile.fromFileSync(image.path,
+              filename: image.path.split(Platform.pathSeparator).last)));
+    }
+    if (categoryId != null) {
+      _data.fields.add(MapEntry('category_id', categoryId.toString()));
+    }
+    if (id != null) {
+      _data.fields.add(MapEntry('id', id.toString()));
+    }
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'product/$prodId',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'POST',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<dynamic>.fromJson(_result.data);
+    return value;
+  }
+
+  @override
+  deleteProduct({prodId}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'product/delete/$prodId',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'DELETE',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<dynamic>.fromJson(_result.data);
+    return value;
+  }
 }
