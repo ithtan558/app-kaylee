87dbeaf67907389ab9a11b542e1920edbc429f31
tung <tungpt@sendo.vn>
2020-08-05 16:48:26 +0700

    add customer_service.dart

---
+++ lib/services/src/customer/customer_service.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+import 'package:anth_package/anth_package.dart';
+import 'package:dio/dio.dart';
+import 'package:kaylee/models/models.dart';
+
+part 'customer_service.g.dart';
+
+@RestApi()
+abstract class CustomerService {
+  factory CustomerService(Dio dio) = _CustomerService;
+
+  @GET('customer')
+  Future<ResponseModel<Customers>> getCustomers({
+    @Query('page') int page,
+    @Query('limit') int limit,
+    @Query('keyword') String keyword,
+    @Query('sort') String sort,
+  });
+
+  @GET('customer/{customerId}')
+  Future<ResponseModel<Customer>> getCustomer({@Path() int customerId});
+
+  @POST('customer')
+  @MultiPart()
+  Future<ResponseModel<Customer>> newCustomer({
+    @Part(name: 'first_name') String firstName,
+    @Part(name: 'last_name') String lastName,
+    @Part() int birthday,
+    @Part(name: 'hometown_city_id') int hometownCityId,
+    @Part() int address,
+    @Part(name: 'city_id') int cityId,
+    @Part(name: 'district_id') int districtId,
+    @Part(name: 'wards_id') int wardsId,
+    @Part() String phone,
+    @Part() File image,
+    @Part() String email,
+  });
+
+  @POST('customer/{customerId}')
+  @MultiPart()
+  Future<ResponseModel<Customer>> updateCustomer({
+    @Part(name: 'first_name') String firstName,
+    @Part(name: 'last_name') String lastName,
+    @Part() int birthday,
+    @Part(name: 'hometown_city_id') int hometownCityId,
+    @Part() int address,
+    @Part(name: 'city_id') int cityId,
+    @Part(name: 'district_id') int districtId,
+    @Part(name: 'wards_id') int wardsId,
+    @Part() String phone,
+    @Part() File image,
+    @Part() String email,
+    @Part() int id,
+    @Path() int customerId,
+  });
+
+  @DELETE('customer/delete/{customerId}')
+  Future<ResponseModel> deleteCustomer({@Path() int customerId});
+}
---
+++ lib/services/src/customer/customer_service.g.dart
@@ -0,0 +1,216 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'customer_service.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+class _CustomerService implements CustomerService {
+  _CustomerService(this._dio, {this.baseUrl}) {
+    ArgumentError.checkNotNull(_dio, '_dio');
+  }
+
+  final Dio _dio;
+
+  String baseUrl;
+
+  @override
+  getCustomers({page, limit, keyword, sort}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      r'page': page,
+      r'limit': limit,
+      r'keyword': keyword,
+      r'sort': sort
+    };
+    queryParameters.removeWhere((k, v) => v == null);
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'customer',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<Customers>.fromJson(_result.data);
+    return value;
+  }
+
+  @override
+  getCustomer({customerId}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'customer/$customerId',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<Customer>.fromJson(_result.data);
+    return value;
+  }
+
+  @override
+  newCustomer(
+      {firstName,
+      lastName,
+      birthday,
+      hometownCityId,
+      address,
+      cityId,
+      districtId,
+      wardsId,
+      phone,
+      image,
+      email}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
+    final _data = FormData();
+    if (firstName != null) {
+      _data.fields.add(MapEntry('first_name', firstName));
+    }
+    if (lastName != null) {
+      _data.fields.add(MapEntry('last_name', lastName));
+    }
+    if (birthday != null) {
+      _data.fields.add(MapEntry('birthday', birthday.toString()));
+    }
+    if (hometownCityId != null) {
+      _data.fields.add(MapEntry('hometown_city_id', hometownCityId.toString()));
+    }
+    if (address != null) {
+      _data.fields.add(MapEntry('address', address.toString()));
+    }
+    if (cityId != null) {
+      _data.fields.add(MapEntry('city_id', cityId.toString()));
+    }
+    if (districtId != null) {
+      _data.fields.add(MapEntry('district_id', districtId.toString()));
+    }
+    if (wardsId != null) {
+      _data.fields.add(MapEntry('wards_id', wardsId.toString()));
+    }
+    if (phone != null) {
+      _data.fields.add(MapEntry('phone', phone));
+    }
+    if (image != null) {
+      _data.files.add(MapEntry(
+          'image',
+          MultipartFile.fromFileSync(image.path,
+              filename: image.path.split(Platform.pathSeparator).last)));
+    }
+    if (email != null) {
+      _data.fields.add(MapEntry('email', email));
+    }
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'customer',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'POST',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<Customer>.fromJson(_result.data);
+    return value;
+  }
+
+  @override
+  updateCustomer(
+      {firstName,
+      lastName,
+      birthday,
+      hometownCityId,
+      address,
+      cityId,
+      districtId,
+      wardsId,
+      phone,
+      image,
+      email,
+      id,
+      customerId}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
+    final _data = FormData();
+    if (firstName != null) {
+      _data.fields.add(MapEntry('first_name', firstName));
+    }
+    if (lastName != null) {
+      _data.fields.add(MapEntry('last_name', lastName));
+    }
+    if (birthday != null) {
+      _data.fields.add(MapEntry('birthday', birthday.toString()));
+    }
+    if (hometownCityId != null) {
+      _data.fields.add(MapEntry('hometown_city_id', hometownCityId.toString()));
+    }
+    if (address != null) {
+      _data.fields.add(MapEntry('address', address.toString()));
+    }
+    if (cityId != null) {
+      _data.fields.add(MapEntry('city_id', cityId.toString()));
+    }
+    if (districtId != null) {
+      _data.fields.add(MapEntry('district_id', districtId.toString()));
+    }
+    if (wardsId != null) {
+      _data.fields.add(MapEntry('wards_id', wardsId.toString()));
+    }
+    if (phone != null) {
+      _data.fields.add(MapEntry('phone', phone));
+    }
+    if (image != null) {
+      _data.files.add(MapEntry(
+          'image',
+          MultipartFile.fromFileSync(image.path,
+              filename: image.path.split(Platform.pathSeparator).last)));
+    }
+    if (email != null) {
+      _data.fields.add(MapEntry('email', email));
+    }
+    if (id != null) {
+      _data.fields.add(MapEntry('id', id.toString()));
+    }
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'customer/$customerId',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'POST',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<Customer>.fromJson(_result.data);
+    return value;
+  }
+
+  @override
+  deleteCustomer({customerId}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'customer/delete/$customerId',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'DELETE',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = ResponseModel<dynamic>.fromJson(_result.data);
+    return value;
+  }
+}
