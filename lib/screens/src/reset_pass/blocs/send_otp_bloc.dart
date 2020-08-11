a924ad7a5dfb9a40104e4825d752d29efd6405db
tungpham <phamthanhtung1995@gmail.com>
2020-08-11 20:08:19 +0700

    refactor

--- lib/screens/src/reset_pass/blocs/send_otp_bloc.dart
+++ lib/screens/src/reset_pass/blocs/send_otp_bloc.dart
@@ -2,66 +2,29 @@ import 'package:anth_package/anth_package.dart';
 import 'package:kaylee/models/models.dart';
 import 'package:kaylee/services/services.dart';
 
-class SendOtpBloc extends BaseBloc {
+class SendOtpBloc extends Cubit<SingleModel<VerifyPhoneResult>> {
   UserService userService;
 
-  SendOtpBloc(this.userService);
-
-  @override
-  Stream mapEventToState(e) async* {
-    if (e is ErrorEvent) {
-      yield* errorState(e);
-    } else if (e is VerifyPhoneSendOtpEvent) {
-      yield LoadingState();
-      RequestHandler(
-        request: userService?.verifyPhone(VerifyPhoneBody(phone: e.phone)),
-        onSuccess: ({message, result}) {
-          add(SuccessSendOtpEvent(result, message));
-        },
-        onFailed: (code, {error}) {
-          if (error.code.isNotNull) {
-            add(PhoneErrorResetPassScrEvent(error.message));
-          } else {
-            errorEvent(code, error: error);
-          }
-        },
-      );
-    } else if (e is SuccessSendOtpEvent) {
-      yield SuccessSendOtpState(e.result, e.message);
-    } else if (e is PhoneErrorResetPassScrEvent) {
-      yield PhoneErrorSendOtpState(e.message);
-    }
-  }
+  SendOtpBloc({this.userService}) : super(SingleModel());
 
   void verifyPhone({String phone}) {
-    add(VerifyPhoneSendOtpEvent(phone));
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: userService?.verifyPhone(VerifyPhoneBody(phone: phone)),
+      onSuccess: ({message, result}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..item = result
+          ..message = message
+          ..error = null
+          ..code = null));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..error = error
+          ..code = code));
+      },
+    );
   }
-}
-
-class VerifyPhoneSendOtpEvent {
-  final String phone;
-
-  VerifyPhoneSendOtpEvent(this.phone);
-}
-
-class SuccessSendOtpEvent {
-  final VerifyPhoneResult result;
-  final Message message;
-
-  SuccessSendOtpEvent(this.result, this.message);
-}
-
-class PhoneErrorResetPassScrEvent extends MessageErrorEvent {
-  PhoneErrorResetPassScrEvent(String message) : super(message);
-}
-
-class SuccessSendOtpState {
-  final VerifyPhoneResult result;
-  final Message message;
-
-  SuccessSendOtpState(this.result, this.message);
-}
-
-class PhoneErrorSendOtpState extends MessageErrorState {
-  PhoneErrorSendOtpState(String message) : super(message);
 }
