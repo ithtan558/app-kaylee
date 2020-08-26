473678d1017f62e05c4c1ff23c7a1022dc3d5dcf
tungpham <phamthanhtung1995@gmail.com>
2020-08-26 22:45:32 +0700

    load data notify_detail_screen.dart

---
+++ lib/screens/src/notification/detail/bloc/bloc.dart
@@ -0,0 +1,85 @@
+import 'package:anth_package/anth_package.dart' hide Notification, Status;
+import 'package:kaylee/models/models.dart';
+import 'package:kaylee/services/services.dart';
+
+class NotifyDetailScreenBloc extends Cubit<SingleModel> {
+  final NotificationService notificationService;
+  Notification notification;
+
+  NotifyDetailScreenBloc({this.notificationService, this.notification})
+      : super(SingleModel());
+
+  void delete() {
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: notificationService.delete(id: 1
+          // notification.id
+          ),
+      onSuccess: ({message, result}) {
+        emit(DeleteNotificationState.copy(state
+          ..loading = false
+          ..item = notification
+          ..message = message));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..error = error
+          ..code = code));
+      },
+    );
+  }
+
+  void loadDetail() {
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: notificationService.getDetail(
+        id: notification.id,
+        notificationId: notification.id,
+      ),
+      onSuccess: ({message, result}) {
+        if (this.notification.status == Status.notRead) {
+          _updateStatus();
+        }
+        emit(NotificationDetailModel.copy(state
+          ..loading = false
+          ..item = result
+          ..error = null
+          ..code = null));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..error = error
+          ..code = code));
+      },
+    );
+  }
+
+  void _updateStatus() {
+    RequestHandler(
+        request: notificationService.updateStatus(
+            body: NotificationStatusBody(
+      id: notification.id,
+      status: Status.read,
+    )));
+  }
+}
+
+class DeleteNotificationState extends SingleModel {
+  DeleteNotificationState.copy(SingleModel old) {
+    this
+      ..loading = old?.loading
+      ..message = old?.message
+      ..item = old?.item;
+  }
+}
+
+class NotificationDetailModel extends SingleModel<Notification> {
+  NotificationDetailModel.copy(SingleModel old) {
+    this
+      ..loading = old?.loading
+      ..message = old?.message
+      ..item = old?.item;
+  }
+}
--- lib/screens/src/notification/detail/notify_detail_screen.dart
+++ lib/screens/src/notification/detail/notify_detail_screen.dart
@@ -1,11 +1,25 @@
+import 'dart:async';
+
+import 'package:anth_package/anth_package.dart' hide Notification;
 import 'package:core_plugin/core_plugin.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
+import 'package:kaylee/base/kaylee_state.dart';
+import 'package:kaylee/models/models.dart' as models;
 import 'package:kaylee/res/res.dart';
+import 'package:kaylee/screens/src/notification/detail/bloc/bloc.dart';
+import 'package:kaylee/utils/utils.dart';
 import 'package:kaylee/widgets/widgets.dart';
 
 class NotifyDetailScreen extends StatefulWidget {
-  static Widget newInstance() => NotifyDetailScreen._();
+  static Widget newInstance() => BlocProvider<NotifyDetailScreenBloc>(
+        create: (context) => NotifyDetailScreenBloc(
+          notificationService: context.network.provideNotificationService(),
+          notification: context.getArguments<models.Notification>(),
+        ),
+        child: NotifyDetailScreen._(),
+      );
 
   NotifyDetailScreen._();
 
@@ -13,61 +27,107 @@ class NotifyDetailScreen extends StatefulWidget {
   _NotifyDetailScreenState createState() => new _NotifyDetailScreenState();
 }
 
-class _NotifyDetailScreenState extends BaseState<NotifyDetailScreen> {
+class _NotifyDetailScreenState extends KayleeState<NotifyDetailScreen> {
+  NotifyDetailScreenBloc _bloc;
+  StreamSubscription sub;
+
   @override
   void initState() {
     super.initState();
+    _bloc = context.bloc<NotifyDetailScreenBloc>();
+    sub = _bloc.listen((state) {
+      if (state.loading) {
+        showLoading();
+      } else if (!state.loading) {
+        hideLoading();
+      }
+    });
+    _bloc.loadDetail();
   }
 
   @override
   void dispose() {
+    sub.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: KayleeAppBar(
-        leading: FlatButton(
-          shape: CircleBorder(),
-          child: ImageIcon(
-            AssetImage(Images.ic_close),
-            color: ColorsRes.hintText,
-            size: 44,
+    return BlocListener<NotifyDetailScreenBloc, SingleModel>(
+      listener: (context, state) {
+        if (!state.loading) {
+          if (state is DeleteNotificationState) {
+            showKayleeAlertMessageYesDialog(
+              context: context,
+              message: state.message,
+              onPressed: popScreen,
+              onDismiss: () {
+                popScreen(resultBundle: Bundle(state.item));
+              },
+            );
+          } else if (state.code.isNotNull &&
+              state.code != ErrorType.UNAUTHORIZED) {
+            showKayleeAlertErrorYesDialog(
+              context: context,
+              error: state.error,
+              onPressed: popScreen,
+            );
+          }
+        }
+      },
+      child: KayleeScrollview(
+        appBar: KayleeAppBar(
+          leading: FlatButton(
+            shape: CircleBorder(),
+            child: ImageIcon(
+              AssetImage(Images.ic_close),
+              color: ColorsRes.hintText,
+              size: 44,
+            ),
+            onPressed: () {
+              context.pop();
+            },
           ),
-          onPressed: () {
-            context.pop();
-          },
-        ),
-        actions: <Widget>[
-          Container(
-            height: double.infinity,
-            margin: EdgeInsets.only(right: Dimens.px16),
-            alignment: Alignment.centerRight,
-            child: HyperLinkText(
-              text: Strings.xoa,
-              textStyle: TextStyles.hyper16W500,
-              onTap: () {},
-            ),
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(Dimens.px16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              child: KayleeText.normal16W500(
-                'A Beginners Guide To Chinese Cookery',
+          actions: <Widget>[
+            Container(
+              height: double.infinity,
+              margin: EdgeInsets.only(right: Dimens.px16),
+              alignment: Alignment.centerRight,
+              child: HyperLinkText(
+                text: Strings.xoa,
+                textStyle: TextStyles.hyper16W500,
+                onTap: () {
+                  _bloc.delete();
+                },
               ),
-              padding: const EdgeInsets.only(bottom: Dimens.px16),
-            ),
-            KayleeText.hint16W400(
-              'Chopped, sliced or wedged, hard-cooked eggs are the basis of egg salad and add protein and a happy glow to tossed and composed salads and casseroles. Chopped yolks and whites comprise Eggs Goldenrod and Polonaise Sauce. Whole hard-cooked eggs become comforting, familiar deviled eggs or zingy, newly rediscovered pickled eggs. Simply sprinkled with an herb or more fancily coated in sausage for Scotch eggs, hard-cooked eggs are nature’s own hand-held snack food. With a supply of hard-cooked eggs on hand, you’re ready for almost any meal occasion.\n\nHard-cooked eggs are often incorrectly called hard-boiled eggs. Yes, the cooking water must come to a boil. But, you’ll get more tender, less rubbery eggs without a green ring around the yolk and you’ll have less breakage if you turn off the heat or remove the pan from the burner, allowing the eggs to cook gently in hot water.\n\nVery fresh eggs may be difficult to peel. The fresher the eggs, the more the shell membranes cling tenaciously to the shells. The simplest method for easy peeling is to buy and refrigerate eggs a week to 10 days in advance of hard cooking. This brief “breather” allows the eggs to take in air which helps separate the membranes from the shell. 1. Place eggs in single layer in saucepan. Add enough tap water to come at least 1 inch above eggs.\n\n2. Cover. Quickly bring just to boiling. Turn off heat. 3. If necessary, remove pan from burner to prevent further boiling. Let eggs stand, covered, in the hot water about 15 minutes for Large eggs (12 minutes for Medium, 18 for Extra Large.)\n\n4. Immediately run cold water over eggs or place them in ice water (not standing water) until completely cooled. Once cooled, refrigerate eggs in their shells and use within one week of cooking or peel and use immediately.\n\n5. To remove shell, crackle it by tapping gently until a fine network of lines appears all over the shell.\n\n6. Roll egg between hands to loosen shell.\n\n7. Peel, starting at large end. Hold egg under running cold water or dip in bowl of water to help ease off shell. 8. To segment eggs evenly, use an egg slicer or wedger. For chopped eggs, rotate a sliced egg 90 degree in a slicer and slice again. Or chop eggs with a sharp pastry blender in a bowl. Draw down a wedger’s wires only partway to open an egg to hold a stuffing or resemble a flower.',
-              overflow: TextOverflow.visible,
-            ),
+            )
           ],
+        ),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(Dimens.px16),
+          child: BlocBuilder<NotifyDetailScreenBloc, SingleModel>(
+            buildWhen: (previous, current) =>
+            current is NotificationDetailModel,
+            builder: (context, state) {
+              if (state is! NotificationDetailModel) return Container();
+              final data = (state as NotificationDetailModel).item;
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    child: KayleeText.normal16W500(
+                      data.title ?? '',
+                    ),
+                    padding: const EdgeInsets.only(bottom: Dimens.px16),
+                  ),
+                  HtmlWidget(
+                    data.content ?? '',
+                    textStyle: TextStyles.hint16W400,
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
