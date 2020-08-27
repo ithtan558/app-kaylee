4b0b77dc42b4c52074249276ed12a68de9e44cca
tungpham <phamthanhtung1995@gmail.com>
2020-08-27 13:33:27 +0700

    rename

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -4,7 +4,7 @@ export 'src/about/about_screen.dart';
 export 'src/brand/create_new/create_new_brand_screen.dart';
 export 'src/brand/list/brand_list_screen.dart';
 export 'src/cart/cart_screen.dart';
-export 'src/commission_list/commission_list_screen.dart';
+export 'src/commission/commission_list_screen.dart';
 export 'src/customer/create_new/create_new_customer_screen.dart';
 export 'src/customer/list/customer_list_screen.dart';
 export 'src/edit_profile/edit_profile_screen.dart';
