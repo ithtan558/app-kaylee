70470aa03befa1dc0432034856aeca4dc7135e44
tungpham <phamthanhtung1995@gmail.com>
2020-08-27 15:51:41 +0700

    display data on commission_list_screen.dart

---
+++ lib/screens/src/commission/list/bloc/bloc.dart
@@ -0,0 +1,90 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:flutter/foundation.dart';
+import 'package:kaylee/base/kaylee_filter_interface.dart';
+import 'package:kaylee/base/loadmore_interface.dart';
+import 'package:kaylee/models/models.dart';
+import 'package:kaylee/services/services.dart';
+
+class CommissionFilter extends Filter {
+  Brand brand;
+  City city;
+  District district;
+}
+
+class CommissionListScreenBloc extends Cubit<LoadMoreModel<Employee>>
+    implements LoadMoreInterface, KayleeFilterInterface<CommissionFilter> {
+  final EmployeeService employeeService;
+  CommissionFilter _filter;
+
+  CommissionListScreenBloc({@required this.employeeService})
+      : super(LoadMoreModel());
+
+  void loadEmployees() {
+    emit(LoadMoreModel.copy(state..loading = true));
+    RequestHandler(
+      request: employeeService.getEmployees(
+        limit: state.limit,
+        page: state.page,
+        keyword: _filter?.keyword,
+        brandId: _filter?.brand?.id,
+        cityId: _filter?.city?.id,
+        districtIds: _filter?.district?.id?.toString(),
+      ),
+      onSuccess: ({message, result}) {
+        final employees = (result as Employees).items;
+        emit(LoadMoreModel.copy(state
+          ..loading = false
+          ..addAll(employees)
+          ..code = null
+          ..error = null));
+      },
+      onFailed: (code, {error}) {
+        emit(LoadMoreModel.copy(state
+          ..loading = false
+          ..code = code
+          ..error = error));
+      },
+    );
+  }
+
+  @override
+  void loadMore() {
+    state.page++;
+    loadEmployees();
+  }
+
+  @override
+  bool loadWhen() => !state.loading && !state.ended;
+
+  @override
+  void loadFilter() {
+    if (loadFilterWhen) {
+      state
+        ..items = null
+        ..page = 1;
+      loadEmployees();
+    }
+  }
+
+  @override
+  CommissionFilter getFilter() {
+    return _filter;
+  }
+
+  @override
+  void resetFilter() {
+    _filter = null;
+  }
+
+  @override
+  bool get isEmptyFilter => _filter == null;
+
+  @override
+  CommissionFilter updateFilter() {
+    if (isEmptyFilter) _filter = CommissionFilter();
+    return _filter;
+  }
+
+  @override
+  bool get loadFilterWhen => !isEmptyFilter || state.items.isNullOrEmpty;
+}
---
+++ lib/screens/src/commission/list/commission_list_screen.dart
@@ -0,0 +1,113 @@
+import 'dart:async';
+
+import 'package:anth_package/anth_package.dart';
+import 'package:core_plugin/core_plugin.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:kaylee/models/models.dart';
+import 'package:kaylee/res/res.dart';
+import 'package:kaylee/screens/screens.dart';
+import 'package:kaylee/screens/src/commission/list/bloc/bloc.dart';
+import 'package:kaylee/screens/src/staff/list/widgets/staff_item.dart';
+import 'package:kaylee/utils/utils.dart';
+import 'package:kaylee/widgets/widgets.dart';
+
+class CommissionListScreen extends StatefulWidget {
+  static Widget newInstance() => BlocProvider(
+        create: (context) => CommissionListScreenBloc(
+          employeeService: context.network.provideEmployeeService(),
+        ),
+        child: CommissionListScreen._(),
+      );
+
+  CommissionListScreen._();
+
+  @override
+  _CommissionListScreenState createState() => new _CommissionListScreenState();
+}
+
+class _CommissionListScreenState extends BaseState<CommissionListScreen> {
+  CommissionListScreenBloc commissionListScreenBloc;
+  StreamSubscription commissionBlocSub;
+  final dateFilterController = KayleeDateFilterController();
+
+  @override
+  void initState() {
+    super.initState();
+    commissionListScreenBloc = context.bloc<CommissionListScreenBloc>();
+    commissionBlocSub = commissionListScreenBloc.listen((state) {
+      if (state.code.isNotNull) {
+        showKayleeAlertErrorYesDialog(context: context, error: state.error);
+      }
+    });
+    commissionListScreenBloc.loadEmployees();
+  }
+
+  @override
+  void dispose() {
+    commissionBlocSub.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: KayleeAppBar(
+        title: Strings.dsNhanVienNhanHH,
+        actions: [
+          FilterButton<CommissionFilter>(
+            controller: commissionListScreenBloc,
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          KayleeDateFilter(
+            controller: dateFilterController,
+          ),
+          Expanded(
+            child: KayleeLoadMoreHandler(
+              controller: context.bloc<CommissionListScreenBloc>(),
+              child: BlocBuilder<CommissionListScreenBloc,
+                  LoadMoreModel<Employee>>(
+                buildWhen: (previous, current) {
+                  return !current.loading;
+                },
+                builder: (context, state) {
+                  return KayleeGridView(
+                    padding: EdgeInsets.all(Dimens.px16),
+                    childAspectRatio: 103 / 195,
+                    itemBuilder: (c, index) {
+                      final item = state.items.elementAt(index);
+                      return StaffItem(
+                        employee: item,
+                        onTap: () {
+                          pushScreen(PageIntent(
+                              screen: CommissionDetailScreen,
+                              bundle: Bundle(CommissionDetailScreenData(
+                                date: dateFilterController.value,
+                                employee: item,
+                              ))));
+                        },
+                      );
+                    },
+                    itemCount: state.items?.length,
+                    loadingBuilder: (context) {
+                      if (state.ended) return Container();
+                      return Align(
+                        alignment: Alignment.topCenter,
+                        child: CupertinoActivityIndicator(
+                          radius: Dimens.px16,
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
