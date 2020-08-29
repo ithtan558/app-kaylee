cfad30c57913fe6043ab42670e59455882bcbebd
tungpham <phamthanhtung1995@gmail.com>
2020-08-29 22:37:54 +0700

    move into account

--- lib/screens/src/home/home_screen.dart
+++ lib/screens/src/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kaylee/base/kaylee_state.dart';
-import 'package:kaylee/screens/src/home/tabs/account_tab.dart';
+import 'package:kaylee/screens/src/home/tabs/account/account_tab.dart';
 import 'package:kaylee/screens/src/home/tabs/cashier/cashier_tab.dart';
 import 'package:kaylee/screens/src/home/tabs/history_tab.dart';
 import 'package:kaylee/screens/src/home/tabs/home/home_tab.dart';
