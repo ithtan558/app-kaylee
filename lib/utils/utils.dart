b215e046b6b648e7040a7fcdc636e1d2597f9617
tungpham <phamthanhtung1995@gmail.com>
2020-08-31 17:07:09 +0700

    wrong format dateFormat2

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -4,4 +4,4 @@ export 'src/extensions.dart';
 
 const String dateFormat = 'yyyy-MM-dd';
 const String dateFormat1 = 'yyyy-MM-dd HH:mm:ss';
-const String dateFormat2 = 'dd/DD/yyyy';
+const String dateFormat2 = 'dd/MM/yyyy';
