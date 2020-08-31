61e4eccac49c5c898b7ad8e35d41791df1a81680
tungpham <phamthanhtung1995@gmail.com>
2020-08-31 11:36:31 +0700

    add bloc for screen edit_profile_screen.dart

---
+++ lib/screens/src/edit_profile/bloc/edit_profile_bloc.dart
@@ -0,0 +1,71 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:kaylee/models/models.dart';
+import 'package:kaylee/services/services.dart';
+
+class EditProfileBloc extends Cubit<SingleModel<UserInfo>> {
+  final UserService userService;
+  UserInfo userInfo;
+
+  EditProfileBloc({this.userService, this.userInfo}) : super(SingleModel());
+
+  void loadProfile() {
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: userService.getProfile(),
+      onSuccess: ({message, result}) {
+        emit(ProfileModel.copy(state
+          ..loading = false
+          ..item = this.userInfo = result));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..error = error
+          ..code = code));
+      },
+    );
+  }
+
+  void updateProfile() {
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: userService.update(
+        firstName: userInfo.firstName,
+        lastName: userInfo.lastName,
+        birthday: userInfo.birthday,
+        address: userInfo.address,
+        cityId: userInfo.city.id,
+        districtId: userInfo.district.id,
+        wardsId: userInfo.wards.id,
+        image: userInfo.imageFile,
+      ),
+      onSuccess: ({message, result}) {
+        emit(UpdateProfileModel.copy(state
+          ..loading = false
+          ..message = message));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..error = error
+          ..code = code));
+      },
+    );
+  }
+}
+
+class ProfileModel extends SingleModel<UserInfo> {
+  ProfileModel.copy(SingleModel old) {
+    this
+      ..loading = old?.loading
+      ..item = old?.item;
+  }
+}
+
+class UpdateProfileModel extends SingleModel<UserInfo> {
+  UpdateProfileModel.copy(SingleModel old) {
+    this
+      ..loading = old?.loading
+      ..message = old?.message;
+  }
+}
