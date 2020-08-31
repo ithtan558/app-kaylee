099e8b9a027ced5b5caac3c460bbd0c636b532bb
tungpham <phamthanhtung1995@gmail.com>
2020-08-31 12:21:19 +0700

    listen onChange of BlocObserver

--- lib/base/kaylee_bloc_observer.dart
+++ lib/base/kaylee_bloc_observer.dart
@@ -2,12 +2,19 @@ import 'package:anth_package/anth_package.dart';
 
 class KayleeBlocObserver extends BlocObserver {
   final void Function(dynamic currentState, dynamic nextState) transition;
+  final void Function(Cubit cubit, Change change) change;
 
-  KayleeBlocObserver({this.transition});
+  KayleeBlocObserver({this.transition, this.change});
 
   @override
   void onTransition(Cubit cubit, Transition transition) {
     this.transition?.call(transition.currentState, transition?.nextState);
     super.onTransition(cubit, transition);
+  }
+
+  @override
+  void onChange(Cubit cubit, Change change) {
+    this.change?.call(cubit, change);
+    super.onChange(cubit, change);
   }
 }
