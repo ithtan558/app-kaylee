b815579ebf285a5d53b783f584df0364bd0e9f5f
tungpham <phamthanhtung1995@gmail.com>
2020-09-02 15:24:06 +0700

    add KayleeMenuFloatButton

--- lib/widgets/src/kaylee_float_button.dart
+++ lib/widgets/src/kaylee_float_button.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:anth_package/anth_package.dart';
 import 'package:flutter/material.dart';
 import 'package:kaylee/res/res.dart';
+import 'package:kaylee/widgets/widgets.dart';
 
 class KayleeFloatButton extends StatefulWidget {
   final VoidCallback onTap;
@@ -111,7 +114,7 @@ class _KayleeFloatButtonState extends State<KayleeFloatButton>
             builder: (c, child) {
               return Transform.scale(
                 scale:
-                    1 + animController.value * (1 - Dimens.px46 / Dimens.px50),
+                1 + animController.value * (1 - Dimens.px46 / Dimens.px50),
                 child: child,
               );
             },
@@ -123,12 +126,252 @@ class _KayleeFloatButtonState extends State<KayleeFloatButton>
             ),
           ),
           widget.iconWidget ??
-              Icon(
-                widget.icon ?? Icons.add,
-                color: ColorsRes.button,
-                size: Dimens.px24,
-              )
+              (widget.icon.isNotNull
+                  ? ImageIcon(
+                      AssetImage(
+                        widget.icon,
+                      ),
+                      color: ColorsRes.button,
+                      size: Dimens.px16,
+                    )
+                  : Icon(
+                      Icons.add,
+                      color: ColorsRes.button,
+                      size: Dimens.px24,
+                    ))
         ]),
+      ),
+    );
+  }
+}
+
+class KayleeMenuFloatButton extends StatefulWidget {
+  final VoidCallback onMainTap;
+  final VoidCallback onSecondTap;
+
+  KayleeMenuFloatButton({this.onMainTap, this.onSecondTap});
+
+  @override
+  _KayleeMenuFloatButtonState createState() => _KayleeMenuFloatButtonState();
+}
+
+class _KayleeMenuFloatButtonState extends BaseState<KayleeMenuFloatButton>
+    with SingleTickerProviderStateMixin {
+  AnimationController animController;
+  Animation secondBtnAnim;
+  Animation _label2Animation;
+  Animation _label1Animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 150));
+    secondBtnAnim = Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(parent: animController, curve: Interval(0.3, 0.8)));
+    _label2Animation = Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(parent: animController, curve: Interval(0.2, 1)));
+    _label1Animation = Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(parent: _label2Animation, curve: Interval(0.5, 1)));
+  }
+
+  @override
+  void dispose() {
+    animController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        if (animController.isDismissed)
+          return true;
+        else {
+          animController.reverse();
+          return false;
+        }
+      },
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: AnimatedBuilder(
+              animation: animController,
+              builder: (context, child) {
+                if (animController.isDismissed)
+                  return Container();
+                else
+                  return Opacity(
+                    opacity: animController.value,
+                    child: child,
+                  );
+              },
+              child: GestureDetector(
+                onTap: () {
+                  if (animController.isCompleted) {
+                    animController.reverse();
+                  }
+                },
+                child: Container(
+                  color: ColorsRes.dialogDimBg,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            right: Dimens.px32,
+            bottom: Dimens.px32, //24+8
+            child: AnimatedBuilder(
+              animation: secondBtnAnim,
+              builder: (BuildContext context, Widget child) {
+                final value = secondBtnAnim.value;
+                return Container(
+                  width: Dimens.px40,
+                  height: Dimens.px40,
+                  margin: const EdgeInsets.only(bottom: Dimens.px64),
+                  alignment: Alignment.center,
+                  child: Transform.scale(
+                      alignment: Alignment.center,
+                      scale: value,
+                      child: Opacity(opacity: value, child: child)),
+                );
+              },
+              child: Material(
+                type: MaterialType.circle,
+                clipBehavior: Clip.antiAlias,
+                color: Colors.white,
+                child: InkWell(
+                  onTap: () {
+                    animController.reverse();
+                    Future.delayed(Duration(milliseconds: 50), () {
+                      widget.onSecondTap?.call();
+                    });
+                  },
+                  child: Center(
+                    child: ImageIcon(
+                      AssetImage(Images.ic_list),
+                      color: ColorsRes.hintText,
+                      size: Dimens.px16,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: Dimens.px96,
+            right: Dimens.px24,
+            child: AnimatedBuilder(
+              animation: _label1Animation,
+              builder: (context, child) {
+                final value = _label1Animation.value;
+                if (_label1Animation.isDismissed) return Container();
+                return Container(
+                  margin: EdgeInsets.only(
+                      right: Dimens.px28 + (Dimens.px28 + Dimens.px16) * value),
+                  child: Opacity(
+                    opacity: value,
+                    child: child,
+                  ),
+                );
+              },
+              child: _buildLabel(
+                title: 'Quản lý danh mục',
+                onTap: () {
+                  animController.reverse();
+                  Future.delayed(Duration(milliseconds: 50), () {
+                    widget.onSecondTap?.call();
+                  });
+                },
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: Dimens.px32,
+            right: Dimens.px24,
+            child: AnimatedBuilder(
+              animation: _label2Animation,
+              builder: (context, child) {
+                final value = _label2Animation.value;
+                if (_label2Animation.isDismissed) return Container();
+                return Container(
+                  margin: EdgeInsets.only(
+                      right: Dimens.px28 + (Dimens.px28 + Dimens.px16) * value),
+                  child: Opacity(
+                    opacity: value,
+                    child: child,
+                  ),
+                );
+              },
+              child: _buildLabel(
+                title: 'Tạo sản phẩm mới',
+                onTap: () {
+                  animController.reverse();
+                  Future.delayed(Duration(milliseconds: 50), () {
+                    widget.onMainTap?.call();
+                  });
+                },
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: Dimens.px24,
+            right: Dimens.px24,
+            child: KayleeFloatButton(
+              onTap: () {
+                if (animController.isCompleted) {
+                  animController.reverse();
+                  Future.delayed(Duration(milliseconds: 50), () {
+                    widget.onMainTap?.call();
+                  });
+                } else {
+                  animController.forward();
+                }
+              },
+              iconWidget: AnimatedBuilder(
+                animation: animController,
+                builder: (context, child) {
+                  return Transform.rotate(
+                    angle: pi * animController.value,
+                    child: animController.isCompleted
+                        ? Icon(
+                            Icons.add,
+                            size: Dimens.px24,
+                            color: ColorsRes.button,
+                          )
+                        : ImageIcon(
+                            AssetImage(Images.ic_menu),
+                            size: Dimens.px16,
+                            color: ColorsRes.button,
+                          ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLabel({String title, VoidCallback onTap}) {
+    return Material(
+      borderRadius: BorderRadius.circular(Dimens.px5),
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        onTap: () {
+          onTap?.call();
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Dimens.px16)
+              .copyWith(top: Dimens.px11, bottom: Dimens.px10),
+          child: KayleeText.normal16W500(
+            title,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.end,
+          ),
+        ),
       ),
     );
   }
