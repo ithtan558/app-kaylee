c3b515ac13b59fbb2b1937f2150a9424c0085207
tungpham <phamthanhtung1995@gmail.com>
2020-09-03 21:21:37 +0700

    add category_item.dart

---
+++ lib/widgets/src/category_item.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:kaylee/res/res.dart';
+import 'package:kaylee/widgets/src/kaylee_text.dart';
+
+class CategoryItem extends StatelessWidget {
+  final int index;
+  final String name;
+  final VoidCallback onTap;
+
+  CategoryItem({this.index, this.name, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: Dimens.px48,
+      child: Row(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(Dimens.px5),
+                border: Border.fromBorderSide(BorderSide(
+                    width: 1,
+                    color: Color(0xff979797),
+                    style: BorderStyle.solid))),
+            width: Dimens.px48,
+            height: double.infinity,
+            alignment: Alignment.center,
+            child: KayleeText.normal16W500(
+              index?.toString() ?? '',
+              maxLines: 1,
+              overflow: TextOverflow.fade,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: Dimens.px16),
+              child: Material(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(Dimens.px5),
+                    side: BorderSide(
+                      width: 1,
+                      color: Color(0xff979797),
+                    )),
+                color: Colors.white,
+                clipBehavior: Clip.antiAlias,
+                child: InkWell(
+                  onTap: () {
+                    onTap?.call();
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: double.infinity,
+                    padding: const EdgeInsets.only(
+                        left: Dimens.px26, right: Dimens.px28),
+                    child: KayleeText.normal16W500(
+                      name ?? '',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
