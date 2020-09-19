715f7c5833b10fe25c17345ecec0634225208172
tungpham <phamthanhtung1995@gmail.com>
2020-09-19 21:13:57 +0700

    add KayleeApplication

---
+++ android/app/src/main/kotlin/com/kaylee/KayleeApplication.java
@@ -0,0 +1,37 @@
+package com.kaylee;
+
+import io.flutter.app.FlutterApplication;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+
+public class KayleeApplication extends FlutterApplication implements PluginRegistry.PluginRegistrantCallback {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FlutterFirebaseMessagingService.setPluginRegistrant(this);
+    }
+
+    @Override
+    public void registerWith(PluginRegistry registry) {
+        FirebaseCloudMessagingPluginRegistrant.registerWith(registry);
+    }
+}
+
+final class FirebaseCloudMessagingPluginRegistrant {
+    public static void registerWith(PluginRegistry registry) {
+        if (alreadyRegisteredWith(registry)) {
+            return;
+        }
+        FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+    }
+
+    private static boolean alreadyRegisteredWith(PluginRegistry registry) {
+        final String key = FirebaseCloudMessagingPluginRegistrant.class.getCanonicalName();
+        if (registry.hasPlugin(key)) {
+            return true;
+        }
+        registry.registrarFor(key);
+        return false;
+    }
+}
