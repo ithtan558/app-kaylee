d56d0f095cc0e088caaf2089a54e6f47cb1d6d60
tungpham <phamthanhtung1995@gmail.com>
2020-09-24 23:47:54 +0700

    add revenue.dart

---
+++ lib/models/src/response/report/revenue/revenue.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'revenue.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Revenue _$RevenueFromJson(Map<String, dynamic> json) {
+  return Revenue(
+    totalValue: json['total_value'] as int,
+  );
+}
+
+Map<String, dynamic> _$RevenueToJson(Revenue instance) => <String, dynamic>{
+      'total_value': instance.totalValue,
+    };
