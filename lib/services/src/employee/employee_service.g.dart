19547016a8f4e01c826cd3fa4fe9cfabcdab36a8
tungpham <phamthanhtung1995@gmail.com>
2020-09-24 23:19:05 +0700

    nothing

--- lib/services/src/employee/employee_service.g.dart
+++ lib/services/src/employee/employee_service.g.dart
@@ -141,22 +141,23 @@ class _EmployeeService implements EmployeeService {
   }
 
   @override
-  updateEmployee({firstName,
-    lastName,
-    birthday,
-    hometownCityId,
-    address,
-    cityId,
-    districtId,
-    wardsId,
-    roleId,
-    brandId,
-    phone,
-    image,
-    email,
-    password,
-    id,
-    employeeId}) async {
+  updateEmployee(
+      {firstName,
+      lastName,
+      birthday,
+      hometownCityId,
+      address,
+      cityId,
+      districtId,
+      wardsId,
+      roleId,
+      brandId,
+      phone,
+      image,
+      email,
+      password,
+      id,
+      employeeId}) async {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     queryParameters.removeWhere((k, v) => v == null);
