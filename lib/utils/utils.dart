27f5fdac1af63159c88b218c8afded557c30da42
tungpham <phamthanhtung1995@gmail.com>
2020-09-26 22:36:01 +0700

    add dateFormat2

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -5,3 +5,4 @@ export 'src/extensions.dart';
 const String dateFormat = 'yyyy-MM-dd';
 const String dateFormat1 = 'yyyy-MM-dd HH:mm:ss';
 const String dateFormat2 = 'dd/MM/yyyy';
+const String dateFormat3 = 'HH:mm';
