e633e872ac7b248325b9efa7cd4938096657c238
tungpham <phamthanhtung1995@gmail.com>
2020-09-26 09:22:28 +0700

    create widget_helper.dart

---
+++ lib/screens/src/revenue/widget/widget_helper.dart
@@ -0,0 +1,23 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:flutter/material.dart';
+import 'package:kaylee/base/kaylee_state.dart';
+import 'package:kaylee/res/res.dart';
+import 'package:kaylee/widgets/widgets.dart';
+
+mixin WidgetHelper<T extends StatefulWidget> on KayleeState<T> {
+  KayleeDatePickerTextController datePickerController =
+      KayleeDatePickerTextController(value: DateTime.now());
+
+  Widget buildLoading() {
+    return Container(
+        padding: const EdgeInsets.symmetric(vertical: Dimens.px16),
+        child: KayleeLoadingIndicator());
+  }
+
+  void showErrorDialog({ErrorType code, Error error}) {
+    if (code.isNotNull && code != ErrorType.UNAUTHORIZED) {
+      showKayleeAlertErrorYesDialog(
+          context: context, error: error, onPressed: popScreen);
+    }
+  }
+}
