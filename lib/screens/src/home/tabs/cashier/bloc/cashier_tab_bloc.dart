a79b5f6842a1d4d107143f3a7718a75ea2e991e5
tungpham <phamthanhtung1995@gmail.com>
2020-09-26 22:37:04 +0700

    add cashier_tab_bloc.dart

---
+++ lib/screens/src/home/tabs/cashier/bloc/cashier_tab_bloc.dart
@@ -0,0 +1,62 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:kaylee/base/kaylee_list_interface.dart';
+import 'package:kaylee/base/loadmore_interface.dart';
+import 'package:kaylee/models/models.dart';
+import 'package:kaylee/services/services.dart';
+
+class CashierTabBloc extends Cubit<LoadMoreModel<Order>>
+    with KayleeListInterfaceMixin
+    implements LoadMoreInterface {
+  final OrderService orderService;
+
+  CashierTabBloc({this.orderService}) : super(LoadMoreModel());
+
+  void _loadOrders() {
+    state.loading = true;
+    RequestHandler(
+      request: orderService?.getOrderCashier(
+        limit: state.limit,
+        page: state.page,
+      ),
+      onSuccess: ({message, result}) {
+        final orders = (result as Orders).items;
+        completeRefresh();
+        emit(LoadMoreModel.copy(state
+          ..loading = false
+          ..addAll(orders)
+          ..code = null
+          ..error = null));
+      },
+      onFailed: (code, {error}) {
+        completeRefresh();
+        emit(LoadMoreModel.copy(state
+          ..loading = false
+          ..code = code
+          ..error = error));
+      },
+    );
+  }
+
+  @override
+  void loadMore() {
+    state.page++;
+    _loadOrders();
+  }
+
+  @override
+  bool loadWhen() => !state.loading && !state.ended;
+
+  @override
+  void loadInitData() {
+    _loadOrders();
+  }
+
+  @override
+  void refresh() {
+    super.refresh();
+    state
+      ..page = 1
+      ..items = [];
+    _loadOrders();
+  }
+}
