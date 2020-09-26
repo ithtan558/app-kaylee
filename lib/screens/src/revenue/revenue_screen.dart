7da9ba787277374bbe45d28d4b57e0ceff041b4d
tungpham <phamthanhtung1995@gmail.com>
2020-09-26 09:25:53 +0700

    provide blocs

--- lib/screens/src/revenue/revenue_screen.dart
+++ lib/screens/src/revenue/revenue_screen.dart
@@ -1,10 +1,33 @@
-import 'package:core_plugin/core_plugin.dart';
+import 'package:anth_package/anth_package.dart';
 import 'package:flutter/material.dart';
+import 'package:kaylee/base/kaylee_state.dart';
 import 'package:kaylee/res/res.dart';
+import 'package:kaylee/screens/src/revenue/bloc/bloc.dart';
+import 'package:kaylee/screens/src/revenue/widget/brand_selection_button.dart';
+import 'package:kaylee/screens/src/revenue/widget/employee_revenue_widget.dart';
+import 'package:kaylee/screens/src/revenue/widget/service_revenue_widget.dart';
+import 'package:kaylee/screens/src/revenue/widget/total_revenue_widget.dart';
+import 'package:kaylee/utils/utils.dart';
 import 'package:kaylee/widgets/widgets.dart';
 
 class RevenueScreen extends StatefulWidget {
-  static Widget newInstance() => RevenueScreen._();
+  static Widget newInstance() => MultiBlocProvider(providers: [
+        BlocProvider(
+          create: (context) => TotalRevenueBloc(
+            reportService: context.network.provideReportService(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => EmployeeRevenueBloc(
+            reportService: context.network.provideReportService(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => ServiceRevenueBloc(
+            reportService: context.network.provideReportService(),
+          ),
+        ),
+      ], child: RevenueScreen._());
 
   RevenueScreen._();
 
@@ -12,7 +35,15 @@ class RevenueScreen extends StatefulWidget {
   _RevenueScreenState createState() => _RevenueScreenState();
 }
 
-class _RevenueScreenState extends BaseState<RevenueScreen> {
+class _RevenueScreenState extends KayleeState<RevenueScreen> {
+  TotalRevenueBloc get _totalRevenueBloc => context.bloc<TotalRevenueBloc>();
+
+  EmployeeRevenueBloc get _employeeRevenueBloc =>
+      context.bloc<EmployeeRevenueBloc>();
+
+  ServiceRevenueBloc get _serviceRevenueBloc =>
+      context.bloc<ServiceRevenueBloc>();
+
   @override
   void initState() {
     super.initState();
@@ -33,163 +64,23 @@ class _RevenueScreenState extends BaseState<RevenueScreen> {
           padding: const EdgeInsets.all(Dimens.px16),
           itemBuilder: (c, index) {
             if (index == 0) {
-              return KayleeFlatButton.filter(
-                child: Row(children: [
-                  Expanded(
-                      child: Container(
-                    alignment: Alignment.center,
-                    child: KayleeText.normalWhite16W400(
-                      'Tất cả chi nhánh',
-                      textAlign: TextAlign.center,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  )),
-                  Image.asset(
-                    Images.ic_triangle_down,
-                    color: Colors.white,
-                    width: Dimens.px16,
-                    height: Dimens.px16,
-                  )
-                ]),
-                background: ColorsRes.button,
-                onPress: () {},
+              return BrandSelectionButton(
+                onChanged: (value) {
+                  _totalRevenueBloc.loadData(brand: value);
+                  _employeeRevenueBloc.loadData(brand: value);
+                  _serviceRevenueBloc.loadData(brand: value);
+                },
               );
-            } else if (index == 1) {
-              return buildBackGroundItem(
-                  header: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                          child: KayleeText.normal16W500(Strings.doanhThu)),
-                      KayleeDatePickerText(
-                        onSelect: (changed) {},
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            top: Dimens.px16,
-                            left: Dimens.px16,
-                            right: Dimens.px16,
-                            bottom: Dimens.px8),
-                        child: KayleeText.hint16W400(
-                          'Tổng doanh thu',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            left: Dimens.px16,
-                            right: Dimens.px16,
-                            bottom: Dimens.px16),
-                        child: KayleePriceText.noUnitNormal26W700(
-                          29000000,
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          buildTotalRevenue(title: 'Tiền mặt', price: 19000000),
-                          buildTotalRevenue(
-                              title: 'Tài khoản', price: 10000000),
-                        ],
-                      )
-                    ],
-                  ));
-            } else
-              return buildBackGroundItem(
-                  header: Row(
-                    children: [
-                      Expanded(
-                        child: KayleeText.normal16W500(
-                          'Doanh thu mỗi nhân viên',
-                          maxLines: 1,
-                        ),
-                      ),
-                      KayleeDatePickerText(
-                        onSelect: (changed) {},
-                      ),
-                    ],
-                  ),
-                  child: ListView.separated(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemBuilder: (c, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(Dimens.px16),
-                          child: Row(
-                            children: <Widget>[
-                              Expanded(
-                                child: KayleeText.normal16W500(
-                                  'Maria',
-                                ),
-                              ),
-                              KayleePriceText.noUnitNormal16W400(200000),
-                              Padding(
-                                padding:
-                                const EdgeInsets.only(left: Dimens.px4),
-                                child: KayleeText.hint16W400(Strings.vnd),
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                      separatorBuilder: (c, index) {
-                        return Container(
-                            height: Dimens.px1,
-                            decoration:
-                                BoxDecoration(color: ColorsRes.divider));
-                      },
-                      itemCount: 5));
+            }
+            if (index == 1) return TotalRevenueWidget();
+            if (index == 2) return EmployeeRevenueWidget();
+            return ServiceRevenueWidget();
           },
-          separatorBuilder: (_, index) => Container(
+          separatorBuilder: (_, index) =>
+              Container(
                 height: Dimens.px16,
               ),
-          itemCount: 3),
-    );
-  }
-
-  buildBackGroundItem({Widget header, Widget child}) {
-    return KayleeCartView(
-      borderRadius: BorderRadius.circular(Dimens.px10),
-      child: Material(
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(Dimens.px16),
-              color: ColorsRes.color2,
-              child: header ?? Container(),
-            ),
-            child ?? Container(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildTotalRevenue({String title, dynamic price}) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.only(top: Dimens.px21, bottom: Dimens.px20),
-        child: Column(
-          children: [
-            KayleeText.hint16W400(
-              title ?? '',
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center,
-            ),
-            KayleePriceText.noUnitNormal18W700(
-              price,
-            )
-          ],
-        ),
-      ),
+          itemCount: 4),
     );
   }
 }
