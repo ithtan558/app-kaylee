ae879736e801eab1aeba2ad1398577a1f10d362a
tungpham <phamthanhtung1995@gmail.com>
2020-09-26 08:43:29 +0700

    add revenue_item.dart

---
+++ lib/screens/src/revenue/widget/revenue_item.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:kaylee/res/res.dart';
+import 'package:kaylee/widgets/widgets.dart';
+
+class RevenueItem extends StatelessWidget {
+  final String title;
+  final dynamic price;
+
+  RevenueItem({this.title, this.price});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(Dimens.px16),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: KayleeText.normal16W500(
+              title ?? '',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          KayleePriceText.noUnitNormal16W400(price),
+          Padding(
+            padding: const EdgeInsets.only(left: Dimens.px4),
+            child: KayleeText.hint16W400(Strings.vnd),
+          ),
+        ],
+      ),
+    );
+  }
+}
