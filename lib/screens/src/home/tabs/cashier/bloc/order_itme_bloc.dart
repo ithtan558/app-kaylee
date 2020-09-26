dc4e7f7205e7e9f1541024f16cc375bc1acfcba3
tungpham <phamthanhtung1995@gmail.com>
2020-09-26 23:12:10 +0700

    add order_itme_bloc.dart

---
+++ lib/screens/src/home/tabs/cashier/bloc/order_itme_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:kaylee/models/models.dart';
+import 'package:kaylee/services/services.dart';
+
+class OrderItemBloc extends Cubit<SingleModel> {
+  final OrderService orderService;
+  final Order order;
+
+  OrderItemBloc({this.orderService, this.order}) : super(SingleModel());
+
+  void cancelOrder() {
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: orderService.updateOrderStatus(
+          orderId: order.id,
+          body: UpdateOrderStatusBody(
+            id: order.id,
+            status: OrderStatus.cancel,
+          )),
+      onSuccess: ({message, result}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..message = message));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..error = error
+          ..code = code));
+      },
+    );
+  }
+}
