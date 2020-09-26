3feed619c53f228278b4e663093758eec1152d45
tungpham <phamthanhtung1995@gmail.com>
2020-09-26 08:46:19 +0700

    add employee_revenue_widget.dart

---
+++ lib/screens/src/revenue/widget/employee_revenue_widget.dart
@@ -0,0 +1,78 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:flutter/material.dart';
+import 'package:kaylee/base/kaylee_state.dart';
+import 'package:kaylee/models/models.dart';
+import 'package:kaylee/res/res.dart';
+import 'package:kaylee/screens/src/revenue/bloc/bloc.dart';
+import 'package:kaylee/screens/src/revenue/widget/revenue_item.dart';
+import 'package:kaylee/screens/src/revenue/widget/widget_helper.dart';
+import 'package:kaylee/widgets/widgets.dart';
+
+class EmployeeRevenueWidget extends StatefulWidget {
+  EmployeeRevenueWidget();
+
+  @override
+  _EmployeeRevenueWidgetState createState() => _EmployeeRevenueWidgetState();
+}
+
+class _EmployeeRevenueWidgetState extends KayleeState<EmployeeRevenueWidget>
+    with WidgetHelper<EmployeeRevenueWidget> {
+  EmployeeRevenueBloc get _employeeRevenueBloc =>
+      context.bloc<EmployeeRevenueBloc>();
+
+  @override
+  void initState() {
+    super.initState();
+    _employeeRevenueBloc.loadData(
+      date: datePickerController.value,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KayleeHeaderCard(
+        header: Row(
+          children: [
+            Expanded(
+              child: KayleeText.normal16W500(
+                'Doanh thu mỗi nhân viên',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            KayleeDatePickerText(
+              onSelect: (changed) {
+                _employeeRevenueBloc.loadData(date: changed);
+              },
+              controller: datePickerController,
+            ),
+          ],
+        ),
+        child: BlocConsumer<EmployeeRevenueBloc,
+            SingleModel<List<EmployeeRevenue>>>(
+          listener: (context, state) {
+            showErrorDialog(code: state.code, error: state.error);
+          },
+          builder: (context, state) {
+            if (state.loading) return buildLoading();
+            if (state.code.isNotNull) return SizedBox();
+            return ListView.separated(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (c, index) {
+                  final item = state.item.elementAt(index);
+                  return RevenueItem(
+                    title: item.employee.name,
+                    price: item.amount,
+                  );
+                },
+                separatorBuilder: (c, index) {
+                  return Container(
+                      height: Dimens.px1,
+                      decoration: BoxDecoration(color: ColorsRes.divider));
+                },
+                itemCount: state.item?.length ?? 0);
+          },
+        ));
+  }
+}
