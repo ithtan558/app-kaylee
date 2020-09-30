a130a2fbe8ea10a744fe4a3c93f80daaa1582fa4
tungpham <phamthanhtung1995@gmail.com>
2020-09-30 23:25:53 +0700

    refactor

--- lib/screens/src/order_detail/widgets/select_service_item_list/select_service_list.dart
+++ lib/screens/src/order_detail/widgets/select_service_item_list/select_service_list.dart
@@ -13,24 +13,36 @@ import 'package:kaylee/utils/utils.dart';
 import 'package:kaylee/widgets/widgets.dart';
 
 class SelectServiceList extends StatefulWidget {
-  static Widget newInstance() => MultiBlocProvider(providers: [
-        BlocProvider(
-          create: (context) => SelectServiceCateBloc(
-            servService: context.network.provideServService(),
-          ),
-        ),
-        BlocProvider(
-          create: (context) => SelectServiceListBloc(
-            servService: context.network.provideServService(),
-          ),
-        ),
-      ], child: SelectServiceList._());
+  static Widget newInstance(
+          {List<Service> initialValue,
+          ValueChanged<List<Service>> onConfirm,
+          VoidCallback onCancel}) =>
+      MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => SelectServiceCateBloc(
+                servService: context.network.provideServService(),
+              ),
+            ),
+            BlocProvider(
+              create: (context) => SelectServiceListBloc(
+                servService: context.network.provideServService(),
+                initialData: initialValue,
+              ),
+            ),
+          ],
+          child: SelectServiceList._(
+            onConfirm: onConfirm,
+            onCancel: onCancel,
+          ));
 
-  SelectServiceList._();
+  final ValueChanged<List<Service>> onConfirm;
+  final VoidCallback onCancel;
+
+  SelectServiceList._({this.onConfirm, this.onCancel});
 
   @override
-  _SelectServiceListState createState() =>
-      _SelectServiceListState();
+  _SelectServiceListState createState() => _SelectServiceListState();
 }
 
 class _SelectServiceListState extends KayleeState<SelectServiceList> {
@@ -39,7 +51,7 @@ class _SelectServiceListState extends KayleeState<SelectServiceList> {
 
   SelectServiceListBloc get serviceListBloc =>
       context.bloc<SelectServiceListBloc>();
-  StreamSubscription prodListBlocSub;
+  StreamSubscription _sub;
 
   @override
   void initState() {
@@ -65,7 +77,7 @@ class _SelectServiceListState extends KayleeState<SelectServiceList> {
         }
       }
     });
-    prodListBlocSub = serviceListBloc.listen((state) {
+    _sub = serviceListBloc.listen((state) {
       if (state.code.isNotNull && state.code != ErrorType.UNAUTHORIZED) {
         showKayleeAlertErrorYesDialog(context: context, error: state.error);
       }
@@ -76,7 +88,7 @@ class _SelectServiceListState extends KayleeState<SelectServiceList> {
   @override
   void dispose() {
     cateBlocSub.cancel();
-    prodListBlocSub.cancel();
+    _sub.cancel();
     super.dispose();
   }
 
@@ -101,55 +113,113 @@ class _SelectServiceListState extends KayleeState<SelectServiceList> {
           return KayleeTabBar(
             padding: const EdgeInsets.symmetric(horizontal: Dimens.px8),
             itemCount: categories?.length,
-            mapTitle: (index) => categories.elementAt(index).name,
+            mapTitle: (index) =>
+            categories
+                .elementAt(index)
+                .name,
             onSelected: (value) {
               serviceListBloc.changeTab(
-                  cateId: cateBloc.state.item.elementAt(value).id);
+                  cateId: cateBloc.state.item
+                      .elementAt(value)
+                      .id);
             },
           );
         },
-      ),
-      pageView: KayleeRefreshIndicator(
-        controller: serviceListBloc,
-        child: KayleeLoadMoreHandler(
-          controller: serviceListBloc,
-          child: BlocConsumer<SelectServiceListBloc, LoadMoreModel<Service>>(
-            listener: (context, state) {
-              if (state.code.isNotNull &&
-                  state.code != ErrorType.UNAUTHORIZED) {
-                showKayleeAlertErrorYesDialog(
-                  context: context,
-                  error: state.error,
-                  onPressed: popScreen,
-                );
-              }
-            },
-            builder: (context, state) {
-              return KayleeGridView(
-                padding: EdgeInsets.all(Dimens.px8),
-                childAspectRatio: 103 / 195,
-                itemBuilder: (c, index) {
-                  final item = state.items.elementAt(index);
-                  return KayleeProdItemView.canSelect(
-                    data: KayleeProdItemData(
-                        name: item.name, image: item.image, price: item.price),
-                    onSelect: (selected) {},
-                  );
-                },
-                itemCount: state.items?.length,
-                loadingBuilder: (context) {
-                  if (state.ended) return Container();
-                  return Align(
-                    alignment: Alignment.topCenter,
-                    child: CupertinoActivityIndicator(
-                      radius: Dimens.px16,
-                    ),
-                  );
-                },
-              );
-            },
           ),
-        ),
+      pageView: Column(
+        children: [
+          Expanded(
+            child: KayleeRefreshIndicator(
+              controller: serviceListBloc,
+              child: KayleeLoadMoreHandler(
+                controller: serviceListBloc,
+                child:
+                BlocConsumer<SelectServiceListBloc, LoadMoreModel<Service>>(
+                  listener: (context, state) {
+                    if (state.code.isNotNull &&
+                        state.code != ErrorType.UNAUTHORIZED) {
+                      showKayleeAlertErrorYesDialog(
+                        context: context,
+                        error: state.error,
+                        onPressed: popScreen,
+                      );
+                    }
+                  },
+                  builder: (context, state) {
+                    return KayleeGridView(
+                      padding: EdgeInsets.all(Dimens.px8),
+                      childAspectRatio: 103 / 195,
+                      itemBuilder: (c, index) {
+                        final item = state.items.elementAt(index);
+                        return KayleeProdItemView.canSelect(
+                          data: KayleeProdItemData(
+                              name: item.name,
+                              image: item.image,
+                              price: item.price),
+                          onSelect: (selected) {
+                            serviceListBloc.select(service: item);
+                          },
+                          selected: item.selected,
+                        );
+                      },
+                      itemCount: state.items?.length,
+                      loadingBuilder: (context) {
+                        if (state.ended) return Container();
+                        return Align(
+                          alignment: Alignment.topCenter,
+                          child: CupertinoActivityIndicator(
+                            radius: Dimens.px16,
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(Dimens.px16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: KayLeeRoundedButton.button2(
+                    text: Strings.huy,
+                    margin: const EdgeInsets.only(right: Dimens.px8),
+                    onPressed: () {
+                      widget.onCancel?.call();
+                      popScreen();
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: BlocBuilder<SelectServiceListBloc,
+                      LoadMoreModel<Service>>(
+                    builder: (context, state) {
+                      final enable =
+                          (serviceListBloc.selectedServices?.length ?? 0) > 0 &&
+                              (state.items?.length ?? 0) > 0;
+                      return enable
+                          ? KayLeeRoundedButton.normal(
+                        text: Strings.xacNhan,
+                        margin: const EdgeInsets.only(left: Dimens.px8),
+                        onPressed: () {
+                          widget.onConfirm
+                              ?.call(serviceListBloc.selectedServices);
+                          popScreen();
+                        },
+                      )
+                          : KayLeeRoundedButton.button3(
+                        text: Strings.xacNhan,
+                        margin: const EdgeInsets.only(left: Dimens.px8),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
