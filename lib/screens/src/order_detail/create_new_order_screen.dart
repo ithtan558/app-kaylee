5d9614189a19d03657c8077894093f184df4d4bd
tungpham <phamthanhtung1995@gmail.com>
2020-10-01 17:42:22 +0700

    create new order

--- lib/screens/src/order_detail/create_new_order_screen.dart
+++ lib/screens/src/order_detail/create_new_order_screen.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+
 import 'package:anth_package/anth_package.dart';
 import 'package:flutter/material.dart';
 import 'package:kaylee/app_bloc.dart';
+import 'package:kaylee/base/kaylee_state.dart';
+import 'package:kaylee/base/reload_bloc.dart';
 import 'package:kaylee/components/components.dart';
 import 'package:kaylee/models/models.dart' hide OrderItem;
 import 'package:kaylee/res/res.dart';
+import 'package:kaylee/screens/src/home/tabs/cashier/cashier_tab.dart';
 import 'package:kaylee/screens/src/order_detail/bloc/order_detail_bloc.dart';
 import 'package:kaylee/screens/src/order_detail/widgets/select_customer_field.dart';
 import 'package:kaylee/screens/src/order_detail/widgets/select_order_item_list.dart';
@@ -16,12 +21,12 @@ class NewOrderScreenData {
   NewOrderScreenData({this.openFrom});
 }
 
-enum OrderScreenOpenFrom { detailButton, addNewButton }
+enum OrderScreenOpenFrom { detailButton, addNewButton, addNewFromReservation }
 
 class CreateNewOrderScreen extends StatefulWidget {
   static Widget newInstance() => RepositoryProvider(
     create: (context) => CartModule.init(),
-        child: MultiBlocProvider(
+    child: MultiBlocProvider(
           providers: [
             BlocProvider(
               create: (context) => OrderDetailBloc(
@@ -33,8 +38,8 @@ class CreateNewOrderScreen extends StatefulWidget {
             BlocProvider(create: (context) => CartBloc()),
           ],
           child: CreateNewOrderScreen._(),
-        ),
-      );
+    ),
+  );
 
   CreateNewOrderScreen._();
 
@@ -42,17 +47,70 @@ class CreateNewOrderScreen extends StatefulWidget {
   _CreateNewOrderScreenState createState() => _CreateNewOrderScreenState();
 }
 
-class _CreateNewOrderScreenState extends BaseState<CreateNewOrderScreen> {
+class _CreateNewOrderScreenState extends KayleeState<CreateNewOrderScreen> {
   OrderScreenOpenFrom openFrom;
   final brandController = PickInputController<Brand>();
   final employeeController = PickInputController<Employee>();
   final customerController = SelectCustomerController();
+  final discountController = TextEditingController();
+
+  CartModule get _cart => context.cart;
+
+  OrderRequest get _order => _cart.getOrder();
+
+  OrderDetailBloc get _bloc => context.bloc<OrderDetailBloc>();
+
+  CartBloc get _cartBloc => context.bloc<CartBloc>();
+  StreamSubscription _sub;
 
   @override
   void initState() {
     super.initState();
-    final data = context.bundle.args as NewOrderScreenData;
+
+    _sub = _bloc.listen((state) {
+      if (state.loading)
+        showLoading();
+      else if (!state.loading) {
+        hideLoading();
+        if (state.code.isNotNull && state.code != ErrorType.UNAUTHORIZED) {
+          showKayleeAlertErrorYesDialog(
+              context: context, error: state.error, onPressed: popScreen);
+        } else if (state is UpdateOrderState) {
+          showKayleeAlertMessageYesDialog(
+            context: context,
+            message: state.message,
+            onPressed: popScreen,
+            onDismiss: () {
+              context.bloc<ReloadBloc>().reload(widget: CashierTab);
+            },
+          );
+        } else if (state is CreateOrderState) {
+          showKayleeAlertMessageYesDialog(
+            context: context,
+            message: state.message,
+            onPressed: popScreen,
+            onDismiss: () {
+              context.bloc<ReloadBloc>().reload(widget: CashierTab);
+              popScreen();
+            },
+          );
+        }
+      }
+    });
+
+    final data = context.getArguments<NewOrderScreenData>();
     openFrom = data?.openFrom;
+
+    if (openFrom == OrderScreenOpenFrom.detailButton) {
+      _bloc.get();
+    }
+  }
+
+  @override
+  void dispose() {
+    discountController.dispose();
+    _sub.cancel();
+    super.dispose();
   }
 
   @override
@@ -60,64 +118,115 @@ class _CreateNewOrderScreenState extends BaseState<CreateNewOrderScreen> {
     return UnFocusWidget(
       child: Scaffold(
         appBar: KayleeAppBar.hyperTextAction(
-          title: openFrom == OrderScreenOpenFrom.detailButton
-              ? Strings.chinhSuaDonHang
-              : Strings.taoDonHangMoi,
-          actionTitle: openFrom == OrderScreenOpenFrom.detailButton
-              ? Strings.luu
-              : Strings.tao,
-          onActionClick: () {},
+          title: openFrom == OrderScreenOpenFrom.addNewButton ||
+              openFrom == OrderScreenOpenFrom.addNewFromReservation
+              ? Strings.taoDonHangMoi
+              : Strings.chinhSuaDonHang,
+          actionTitle: openFrom == OrderScreenOpenFrom.addNewButton ||
+              openFrom == OrderScreenOpenFrom.addNewFromReservation
+              ? Strings.tao
+              : Strings.luu,
+          onActionClick: () {
+            if (openFrom == OrderScreenOpenFrom.addNewButton ||
+                openFrom == OrderScreenOpenFrom.addNewFromReservation) {
+              _cart.updateOrderInfo(OrderRequest(
+                brand: brandController.value,
+                employee: employeeController.value,
+              ));
+              _bloc.create();
+            } else {
+              _cart.updateOrderInfo(OrderRequest(
+                brand: brandController.value,
+                employee: employeeController.value,
+              ));
+              _bloc.update();
+            }
+          },
         ),
         body: BlocConsumer<OrderDetailBloc, SingleModel<OrderRequest>>(
-          listener: (context, state) {},
+          listener: (context, state) {
+            customerController.customer = _order?.customer;
+            brandController.value = _order?.brand;
+            employeeController.value = _order?.employee;
+            discountController.text = _order?.discount?.toString();
+          },
           builder: (context, state) {
-            return SingleChildScrollView(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(Dimens.px16),
-                    child: SelectCustomerField(
-                      controller: customerController,
-                      onSelect: (value) {},
+            return Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(Dimens.px16),
+                          child: SelectCustomerField(
+                            controller: customerController,
+                            onSelect: (value) {
+                              _cart.updateOrderInfo(OrderRequest(
+                                customer: customerController.customer,
+                              ));
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: Dimens.px16,
+                              right: Dimens.px16,
+                              bottom: Dimens.px16),
+                          child: KayleePickerTextField(
+                            title: Strings.chiNhanh,
+                            hint: Strings.chonChiNhanhTrongDs,
+                            controller: brandController,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: Dimens.px16,
+                              right: Dimens.px16,
+                              bottom: Dimens.px16),
+                          child: KayleePickerTextField(
+                            title: Strings.nhanVienThucThien,
+                            hint: Strings.chonNhanVienTrongDs,
+                            controller: employeeController,
+                          ),
+                        ),
+                        SelectOrderItemList(),
+                        if (openFrom !=
+                            OrderScreenOpenFrom.addNewFromReservation) ...[
+                          LabelDividerView(title: Strings.giamGia),
+                          Padding(
+                            padding: const EdgeInsets.all(Dimens.px16),
+                            child: KayleeTextField.unitSelection(
+                              hint: '0',
+                              controller: discountController,
+                              textInputAction: TextInputAction.done,
+                              onChange: (value) {
+                                _order.discount = int.tryParse(value);
+                                _cartBloc.updateCart();
+                              },
+                            ),
+                          ),
+                        ]
+                      ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: Dimens.px16,
-                        right: Dimens.px16,
-                        bottom: Dimens.px16),
-                    child: KayleePickerTextField(
-                      title: Strings.chiNhanh,
-                      hint: Strings.chonChiNhanhTrongDs,
-                      controller: brandController,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: Dimens.px16,
-                        right: Dimens.px16,
-                        bottom: Dimens.px16),
-                    child: KayleePickerTextField(
-                      title: Strings.nhanVienThucThien,
-                      hint: Strings.chonNhanVienTrongDs,
-                      controller: employeeController,
-                    ),
-                  ),
-                  SelectOrderItemList(),
-                  LabelDividerView(title: Strings.giamGia),
-                  Padding(
-                    padding: const EdgeInsets.all(Dimens.px16),
-                    child: KayleeTextField.unitSelection(
-                      hint: '0',
-                    ),
-                  ),
-                  KayLeeRoundedButton.normal(
-                    text: Strings.thanhToan,
-                    margin: const EdgeInsets.all(Dimens.px8),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
+                ),
+                if (openFrom != OrderScreenOpenFrom.addNewFromReservation)
+                  BlocBuilder<CartBloc, CartState>(
+                    builder: (context, state) {
+                      if (_order?.cartItems.isNullOrEmpty)
+                        return KayLeeRoundedButton.button3(
+                          text: Strings.thanhToan,
+                          margin: const EdgeInsets.all(Dimens.px8),
+                        );
+                      return KayLeeRoundedButton.normal(
+                        text: Strings.thanhToan,
+                        margin: const EdgeInsets.all(Dimens.px8),
+                        onPressed: () {},
+                      );
+                    },
+                  )
+              ],
             );
           },
         ),
