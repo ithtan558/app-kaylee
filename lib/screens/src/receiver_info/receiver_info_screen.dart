3fa561bb11ea8066e5c1a8c8b05be9ed356b18d3
tungpham <phamthanhtung1995@gmail.com>
2020-10-04 18:53:38 +0700

    show brand picker
    auto fill

--- lib/screens/src/receiver_info/receiver_info_screen.dart
+++ lib/screens/src/receiver_info/receiver_info_screen.dart
@@ -1,13 +1,21 @@
+import 'dart:async';
+
+import 'package:anth_package/anth_package.dart';
 import 'package:core_plugin/core_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:kaylee/base/kaylee_state.dart';
 import 'package:kaylee/models/models.dart';
 import 'package:kaylee/res/res.dart';
 import 'package:kaylee/screens/screens.dart';
+import 'package:kaylee/screens/src/receiver_info/bloc/auto_fill_bloc.dart';
 import 'package:kaylee/utils/utils.dart';
 import 'package:kaylee/widgets/widgets.dart';
 
 class ReceiverInfoScreen extends StatefulWidget {
-  static Widget newInstance() => ReceiverInfoScreen._();
+  static Widget newInstance() => BlocProvider(
+      create: (context) =>
+          AutoFillBloc(service: context.network.provideBrandService()),
+      child: ReceiverInfoScreen._());
 
   ReceiverInfoScreen._();
 
@@ -15,7 +23,8 @@ class ReceiverInfoScreen extends StatefulWidget {
   _ReceiverInfoScreenState createState() => new _ReceiverInfoScreenState();
 }
 
-class _ReceiverInfoScreenState extends BaseState<ReceiverInfoScreen> {
+class _ReceiverInfoScreenState extends KayleeState<ReceiverInfoScreen> {
+  final brandController = PickInputController<Brand>();
   final nameTfController = TextEditingController();
   final phoneTfController = TextEditingController();
   final noteTfController = TextEditingController();
@@ -23,22 +32,59 @@ class _ReceiverInfoScreenState extends BaseState<ReceiverInfoScreen> {
   final phoneFocus = FocusNode();
   final noteFocus = FocusNode();
   KayleeFullAddressController addressController;
+  StreamSubscription _autoFillBlocSub;
 
   @override
   void initState() {
     super.initState();
-    nameTfController.text = context.cart.getOrder().cartSuppInfo?.name;
+    nameTfController.text = context.cart
+        .getOrder()
+        .cartSuppInfo
+        ?.name;
     addressController = KayleeFullAddressController(
-        initAddress: context.cart.getOrder().cartSuppInfo?.address,
-        initCity: context.cart.getOrder().cartSuppInfo?.city,
-        initDistrict: context.cart.getOrder().cartSuppInfo?.district,
-        initWard: context.cart.getOrder().cartSuppInfo?.ward);
-    phoneTfController.text = context.cart.getOrder().cartSuppInfo?.phone;
-    noteTfController.text = context.cart.getOrder().cartSuppInfo?.note;
+        initAddress: context.cart
+            .getOrder()
+            .cartSuppInfo
+            ?.address,
+        initCity: context.cart
+            .getOrder()
+            .cartSuppInfo
+            ?.city,
+        initDistrict: context.cart
+            .getOrder()
+            .cartSuppInfo
+            ?.district,
+        initWard: context.cart
+            .getOrder()
+            .cartSuppInfo
+            ?.ward);
+    phoneTfController.text = context.cart
+        .getOrder()
+        .cartSuppInfo
+        ?.phone;
+    noteTfController.text = context.cart
+        .getOrder()
+        .cartSuppInfo
+        ?.note;
+    brandController.value = context.cart
+        .getOrder()
+        .brand;
+
+    _autoFillBlocSub = context.bloc<AutoFillBloc>().listen((state) {
+      if (state.loading) return showLoading();
+      if (!state.loading) {
+        hideLoading();
+        if (state.code.isNotNull && state.code != ErrorType.UNAUTHORIZED) {
+          showKayleeAlertErrorYesDialog(
+              context: context, error: state.error, onPressed: popScreen);
+        }
+      }
+    });
   }
 
   @override
   void dispose() {
+    _autoFillBlocSub.cancel();
     nameTfController.dispose();
     phoneTfController.dispose();
     noteTfController.dispose();
@@ -58,7 +104,8 @@ class _ReceiverInfoScreenState extends BaseState<ReceiverInfoScreen> {
         ward: addressController.ward,
         phone: phoneTfController.text,
         note: noteTfController.text,
-      ));
+      )
+      ..brand = brandController.value);
   }
 
   @override
@@ -81,6 +128,18 @@ class _ReceiverInfoScreenState extends BaseState<ReceiverInfoScreen> {
               Padding(
                 padding: const EdgeInsets.only(
                     top: Dimens.px16, right: Dimens.px16, left: Dimens.px16),
+                child: KayleePickerTextField(
+                  title: Strings.chiNhanh,
+                  hint: Strings.chonChiNhanhTrongDs,
+                  controller: brandController,
+                  onSelect: (value) {
+                    context.bloc<AutoFillBloc>().loadBrandInfo(brand: value);
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: Dimens.px16, right: Dimens.px16, left: Dimens.px16),
                 child: KayleeTextField.normal(
                   title: Strings.hoVaTen,
                   hint: Strings.nhapHoVaTen,
@@ -89,20 +148,47 @@ class _ReceiverInfoScreenState extends BaseState<ReceiverInfoScreen> {
                   textInputAction: TextInputAction.next,
                 ),
               ),
-              KayleeFullAddressInput(
-                title: Strings.diaChiHienTai,
-                padding: EdgeInsets.all(Dimens.px16),
-                controller: addressController,
+              BlocConsumer<AutoFillBloc, SingleModel<Brand>>(
+                buildWhen: (previous, current) => current is DetailBrandModel,
+                listenWhen: (previous, current) => current is DetailBrandModel,
+                listener: (context, state) {
+                  addressController
+                    ..initAddress =
+                        state.item?.location ?? addressController.initAddress
+                    ..initCity = state.item?.city ?? addressController.initCity
+                    ..initDistrict =
+                        state.item?.district ?? addressController.district
+                    ..initWard =
+                        state.item?.wards ?? addressController.initWard;
+                },
+                builder: (context, state) {
+                  return KayleeFullAddressInput(
+                    title: Strings.diaChiHienTai,
+                    padding: EdgeInsets.all(Dimens.px16),
+                    controller: addressController,
+                  );
+                },
               ),
               Padding(
                 padding: const EdgeInsets.only(
                     left: Dimens.px16, right: Dimens.px16, bottom: Dimens.px16),
-                child: KayleeTextField.phoneInput(
-                  title: Strings.soDienThoai,
-                  controller: phoneTfController,
-                  focusNode: phoneFocus,
-                  nextFocusNode: noteFocus,
-                  textInputAction: TextInputAction.next,
+                child: BlocConsumer<AutoFillBloc, SingleModel<Brand>>(
+                  buildWhen: (previous, current) => current is DetailBrandModel,
+                  listenWhen: (previous, current) =>
+                  current is DetailBrandModel,
+                  listener: (context, state) {
+                    phoneTfController.text =
+                        state.item?.phone ?? phoneTfController.text;
+                  },
+                  builder: (context, state) {
+                    return KayleeTextField.phoneInput(
+                      title: Strings.soDienThoai,
+                      controller: phoneTfController,
+                      focusNode: phoneFocus,
+                      nextFocusNode: noteFocus,
+                      textInputAction: TextInputAction.next,
+                    );
+                  },
                 ),
               ),
               Padding(
@@ -114,7 +200,7 @@ class _ReceiverInfoScreenState extends BaseState<ReceiverInfoScreen> {
                   textInputAction: TextInputAction.newline,
                   controller: noteTfController,
                   fieldHeight:
-                      (context.screenSize.width - Dimens.px32) / (343 / 233),
+                  (context.screenSize.width - Dimens.px32) / (343 / 233),
                   contentPadding: EdgeInsets.symmetric(vertical: Dimens.px16),
                   maxLength: 200,
                   focusNode: noteFocus,
