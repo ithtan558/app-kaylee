8ba913a069f3bac1a9c66cf7cbf747b8658efa09
tungpham <phamthanhtung1995@gmail.com>
2020-10-04 18:40:14 +0700

    add auto_fill_bloc.dart

---
+++ lib/screens/src/receiver_info/bloc/auto_fill_bloc.dart
@@ -0,0 +1,35 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:kaylee/models/models.dart';
+import 'package:kaylee/services/services.dart';
+
+class AutoFillBloc extends Cubit<SingleModel<Brand>> {
+  final BrandService service;
+
+  AutoFillBloc({this.service}) : super(SingleModel());
+
+  void loadBrandInfo({Brand brand}) {
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: service?.getBrand(brandId: brand?.id),
+      onSuccess: ({message, result}) {
+        emit(DetailBrandModel.copy(state
+          ..loading = false
+          ..item = result));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..error = error
+          ..code = code));
+      },
+    );
+  }
+}
+
+class DetailBrandModel extends SingleModel<Brand> {
+  DetailBrandModel.copy(SingleModel old) {
+    this
+      ..loading = old?.loading
+      ..item = old?.item;
+  }
+}
