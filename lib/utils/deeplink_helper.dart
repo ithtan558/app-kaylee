1f138c9d4b483ec614961af74cfb12a247639239
tungpham <phamthanhtung1995@gmail.com>
2020-10-11 21:16:03 +0700

    add deeplink_helper.dart

---
+++ lib/utils/deeplink_helper.dart
@@ -0,0 +1,41 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:kaylee/models/models.dart';
+import 'package:kaylee/screens/screens.dart';
+
+class DeepLinkHelper {
+  static PageIntent handleLink({String link}) {
+    final uri = Uri.tryParse(link);
+    if (uri.isNotNull) {
+      if (uri.path == '/supplier/order/detail') {
+        final orderId = int.tryParse(uri.queryParameters['order_id']);
+        if (orderId.isNotNull) {
+          return PageIntent(
+            screen: MyOrderDetailScreen,
+            bundle: Bundle(
+              Order(id: orderId),
+            ),
+          );
+        }
+      } else if (uri.path == '/product/detail') {
+        final productId = int.tryParse(uri.queryParameters['product_id']);
+        final supplierId = int.tryParse(uri.queryParameters['supplier_id']);
+        if (productId.isNotNull && supplierId.isNotNull) {
+          return PageIntent(
+            screen: ProductDetailScreen,
+            bundle: Bundle(
+              ProductDetailScreenData(
+                product: Product(id: productId),
+                supplier: Supplier(id: supplierId),
+              ),
+            ),
+          );
+        }
+      }
+    }
+    return null;
+  }
+
+  static PageIntent handleNotificationLink({String link}) {
+    return handleLink(link: link) ?? PageIntent(screen: NotificationScreen);
+  }
+}
