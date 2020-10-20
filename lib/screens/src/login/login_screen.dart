656c242296c996ad5397e56c2b5445a8c15fa56f
tungpham <phamthanhtung1995@gmail.com>
2020-10-20 21:52:45 +0700

    add back button

--- lib/screens/src/login/login_screen.dart
+++ lib/screens/src/login/login_screen.dart
@@ -64,133 +64,172 @@ class _LoginScreenState extends KayleeState<LoginScreen> {
   Widget build(BuildContext context) {
     return UnFocusWidget(
         child: Scaffold(
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: Dimens.px16),
-        child: BlocConsumer<LoginScreenBloc, dynamic>(
-          listener: (context, state) async {
-            if (state is LoadingState) {
-              showLoading();
-            } else if (state is ErrorState) {
-              hideLoading();
-              await showKayleeAlertDialog(
-                  context: context,
-                  view:
-                      KayleeAlertDialogView.error(error: state.error, actions: [
-                    KayleeAlertDialogAction.dongY(
-                      onPressed: () {
-                        popScreen();
-                      },
-                    )
-                  ]));
-            } else if (state is PhoneLoginScrErrorState) {
-              hideLoading();
-              _phoneFNode.requestFocus();
-            } else if (state is PassLoginScrErrorState) {
-              hideLoading();
-              _passFNode.requestFocus();
-            } else if (state is SuccessLoginScrState) {
-              hideLoading();
-              context.bloc<AppBloc>().loggedIn(state.result);
-              await showKayleeAlertDialog(
-                context: context,
-                view: KayleeAlertDialogView.message(
-                  message: state.message,
-                  actions: [
-                    KayleeAlertDialogAction.dongY(
-                      onPressed: () {
-                        popScreen();
-                      },
-                    )
-                  ],
-                ),
-                onDismiss: () {
-                  if (data?.openFrom == LoginScreenOpenFrom.LOGIN_DIALOG) {
-                    popScreen();
-                  } else {
-                    context.pushToTop(PageIntent(screen: HomeScreen));
-                  }
-                },
-              );
-            }
-          },
-          builder: (context, state) {
-            return Column(
-              children: [
-                Container(
-                    margin: EdgeInsets.only(top: Dimens.px80),
-                    alignment: Alignment.centerLeft,
-                    child: KayleeText.normal26W700(
-                      Strings.dangNhap,
-                    )),
-                Container(
-                    margin: EdgeInsets.only(top: Dimens.px8),
-                    alignment: Alignment.centerLeft,
-                    child: KayleeText.hint16W400(
-                      Strings.vuiLongDangNhap,
-                    )),
-                Container(
-                    margin: EdgeInsets.only(top: Dimens.px80),
-                    alignment: Alignment.centerLeft,
-                    child: KayleeTextField.phoneInput(
-                      textInputAction: TextInputAction.next,
-                      focusNode: _phoneFNode,
-                      nextFocusNode: _passFNode,
-                      controller: _phoneTController,
-                      error: state is PhoneLoginScrErrorState
-                          ? state.message
-                          : null,
-                    )),
-                Container(
-                    margin: EdgeInsets.only(top: Dimens.px16),
-                    alignment: Alignment.centerLeft,
-                    child: KayleeTextField.password(
-                      controller: _passTController,
-                      textInputAction: TextInputAction.done,
-                      focusNode: _passFNode,
-                      error: state is PassLoginScrErrorState
-                          ? state.message
-                          : null,
-                    )),
-                Container(
-                  margin: EdgeInsets.only(top: Dimens.px16),
-                  child: KayLeeRoundedButton.normal(
-                    onPressed: () async {
-                      bloc.doLogin(LoginBody(
-                        account: _phoneTController.text,
-                        password: _passTController.text,
-                        token: await context
-                            .repository<FirebaseMessaging>()
-                            .getToken(),
-                      ));
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: Dimens.px38),
+                child: SizedBox(
+                  height: Dimens.px56,
+                  child: FlatButton(
+                    onPressed: () {
+                      popScreen();
                     },
-                    margin: EdgeInsets.zero,
-                    text: Strings.dangNhap,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: Dimens.px32),
-                  child: GestureDetector(
-                    onTap: () {
-                      pushScreen(
-                          PageIntent(screen: ResetPassVerifyPhoneScreen));
-                    },
-                    child: Container(
-                      color: Colors.transparent,
-                      child: KayleeText.hyper16W400(
-                        Strings.quenMatKhau,
+                    shape: CircleBorder(),
+                    child: Center(
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            Images.ic_arrow_back,
+                            width: 13,
+                            height: 13,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: Dimens.px4),
+                            child: KayleeText.hyper16W400('Back'),
+                          )
+                        ],
                       ),
                     ),
                   ),
                 ),
-                Container(
-                  margin:
-                      EdgeInsets.only(top: Dimens.px84, bottom: Dimens.px32),
-                  child: Go2RegisterText(),
-                ),
-              ],
-            );
-          },
-        ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.symmetric(horizontal: Dimens.px16),
+              child: BlocConsumer<LoginScreenBloc, dynamic>(
+                listener: (context, state) async {
+                  if (state is LoadingState) {
+                    showLoading();
+                  } else if (state is ErrorState) {
+                    hideLoading();
+                    await showKayleeAlertDialog(
+                        context: context,
+                        view: KayleeAlertDialogView.error(
+                            error: state.error,
+                            actions: [
+                              KayleeAlertDialogAction.dongY(
+                                onPressed: () {
+                                  popScreen();
+                                },
+                              )
+                            ]));
+                  } else if (state is PhoneLoginScrErrorState) {
+                    hideLoading();
+                    _phoneFNode.requestFocus();
+                  } else if (state is PassLoginScrErrorState) {
+                    hideLoading();
+                    _passFNode.requestFocus();
+                  } else if (state is SuccessLoginScrState) {
+                    hideLoading();
+                    context.bloc<AppBloc>().loggedIn(state.result);
+                    await showKayleeAlertDialog(
+                      context: context,
+                      view: KayleeAlertDialogView.message(
+                        message: state.message,
+                        actions: [
+                          KayleeAlertDialogAction.dongY(
+                            onPressed: () {
+                              popScreen();
+                            },
+                          )
+                        ],
+                      ),
+                      onDismiss: () {
+                        if (data?.openFrom ==
+                            LoginScreenOpenFrom.LOGIN_DIALOG) {
+                          popScreen();
+                        } else {
+                          context.pushToTop(PageIntent(screen: HomeScreen));
+                        }
+                      },
+                    );
+                  }
+                },
+                builder: (context, state) {
+                  return Column(
+                    children: [
+                      Container(
+                          margin: EdgeInsets.only(top: 22),
+                          alignment: Alignment.centerLeft,
+                          child: KayleeText.normal26W700(
+                            Strings.dangNhap,
+                          )),
+                      Container(
+                          margin: EdgeInsets.only(top: Dimens.px8),
+                          alignment: Alignment.centerLeft,
+                          child: KayleeText.hint16W400(
+                            Strings.vuiLongDangNhap,
+                          )),
+                      Container(
+                          margin: EdgeInsets.only(top: Dimens.px80),
+                          alignment: Alignment.centerLeft,
+                          child: KayleeTextField.phoneInput(
+                            textInputAction: TextInputAction.next,
+                            focusNode: _phoneFNode,
+                            nextFocusNode: _passFNode,
+                            controller: _phoneTController,
+                            error: state is PhoneLoginScrErrorState
+                                ? state.message
+                                : null,
+                          )),
+                      Container(
+                          margin: EdgeInsets.only(top: Dimens.px16),
+                          alignment: Alignment.centerLeft,
+                          child: KayleeTextField.password(
+                            controller: _passTController,
+                            textInputAction: TextInputAction.done,
+                            focusNode: _passFNode,
+                            error: state is PassLoginScrErrorState
+                                ? state.message
+                                : null,
+                          )),
+                      Container(
+                        margin: EdgeInsets.only(top: Dimens.px16),
+                        child: KayLeeRoundedButton.normal(
+                          onPressed: () async {
+                            bloc.doLogin(LoginBody(
+                              account: _phoneTController.text,
+                              password: _passTController.text,
+                              token: await context
+                                  .repository<FirebaseMessaging>()
+                                  .getToken(),
+                            ));
+                          },
+                          margin: EdgeInsets.zero,
+                          text: Strings.dangNhap,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: Dimens.px32),
+                        child: GestureDetector(
+                          onTap: () {
+                            pushScreen(
+                                PageIntent(screen: ResetPassVerifyPhoneScreen));
+                          },
+                          child: Container(
+                            color: Colors.transparent,
+                            child: KayleeText.hyper16W400(
+                              Strings.quenMatKhau,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(
+                            top: Dimens.px84, bottom: Dimens.px32),
+                        child: Go2RegisterText(),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     ));
   }
