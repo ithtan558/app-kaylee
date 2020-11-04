ecf4bc33f184d9a25848c1f667100a8187d5d8fc
tungpham <phamthanhtung1995@gmail.com>
2020-11-04 23:00:58 +0700

    create `SupplierProdListScreenBloc`

---
+++ lib/screens/src/supplier/product_list/list/bloc/supplier_prod_list_screen_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:kaylee/models/models.dart';
+import 'package:kaylee/services/services.dart';
+
+class SupplierProdListScreenBloc extends Cubit<SingleModel<Supplier>> {
+  final SupplierService service;
+  final Supplier supplier;
+
+  SupplierProdListScreenBloc({
+    this.service,
+    this.supplier,
+  }) : super(SingleModel(item: supplier));
+
+  void getInfo() {
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: service.getSupplierDetail(supplierId: supplier?.id),
+      onSuccess: ({message, result}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..item = result
+          ..error = null
+          ..code = null));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..error = error
+          ..code = code));
+      },
+    );
+  }
+}
