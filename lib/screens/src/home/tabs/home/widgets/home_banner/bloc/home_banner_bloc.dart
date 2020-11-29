56e34ab2f3ca800775aec34c5ef96c6d246cfbf7
tungpham <phamthanhtung1995@gmail.com>
2020-11-30 01:25:25 +0700

    create `HomeBannerBloc`

---
+++ lib/screens/src/home/tabs/home/widgets/home_banner/bloc/home_banner_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:core_plugin/core_plugin.dart';
+import 'package:kaylee/models/models.dart';
+import 'package:kaylee/services/services.dart';
+
+class HomeBannerBloc extends Cubit<SingleModel<List<Banner>>> {
+  final AdvertiseService service;
+
+  HomeBannerBloc({this.service}) : super(SingleModel());
+
+  void loadBanners() {
+    emit(SingleModel.copy(state..loading = true));
+    RequestHandler(
+      request: service.getAllBanners(),
+      onSuccess: ({message, result}) {
+        emit(SingleModel.copy(state
+          ..item = result
+          ..loading = false
+          ..code = null
+          ..error = null));
+      },
+      onFailed: (code, {error}) {
+        emit(SingleModel.copy(state
+          ..loading = false
+          ..code = code
+          ..error));
+      },
+    );
+  }
+}
