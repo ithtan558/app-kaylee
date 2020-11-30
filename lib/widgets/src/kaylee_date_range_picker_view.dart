8c20f5d5657cacd666124468bc1edc37dc62c436
tungpham <phamthanhtung1995@gmail.com>
2020-12-01 00:23:00 +0700

    handle kaylee_date_range_picker_view.dart

--- lib/widgets/src/kaylee_date_range_picker_view.dart
+++ lib/widgets/src/kaylee_date_range_picker_view.dart
@@ -1,15 +1,27 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kaylee/base/kaylee_state.dart';
 import 'package:kaylee/res/res.dart';
 import 'package:kaylee/widgets/widgets.dart';
 
-enum DateRangeValue {
+enum DateRangeValueType {
   thisWeek,
   lastWeek,
-  custom,
 }
 
 class KayleeDateRangePickerView extends StatefulWidget {
+  final ValueChanged<DateTimeRange> onSelectByDate;
+  final ValueChanged<DateRangeValueType> onSelectByType;
+  final DateTimeRange selectedRange;
+  final DateRangeValueType selectedType;
+
+  KayleeDateRangePickerView(
+      {this.onSelectByDate,
+      this.selectedRange,
+      this.onSelectByType,
+      this.selectedType});
+
   @override
   _KayleeDateRangePickerViewState createState() =>
       _KayleeDateRangePickerViewState();
@@ -17,6 +29,22 @@ class KayleeDateRangePickerView extends StatefulWidget {
 
 class _KayleeDateRangePickerViewState
     extends KayleeState<KayleeDateRangePickerView> {
+  DateRangeValueType rangeValueType;
+  DateTimeRange dateRange;
+  DateTime selectedStartDate;
+  DateTime selectedEndDate;
+
+  @override
+  void initState() {
+    super.initState();
+    rangeValueType = widget.selectedType;
+    if (rangeValueType.isNotNull) {
+      dateRange = rangeValueType.range;
+    } else {
+      dateRange = widget.selectedRange;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -28,18 +56,125 @@ class _KayleeDateRangePickerViewState
             textAlign: TextAlign.center,
           ),
         ),
-        SingleChildScrollView(
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Dimens.px16)
+              .copyWith(bottom: Dimens.px16),
           child: Column(
-            children: [],
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: Dimens.px16),
+                child: KayleeText.normal16W500(
+                  Strings.chonNhanh,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: Dimens.px24),
+                child: Row(
+                  children: [
+                    _buildTag(
+                        title: Strings.tuanNay,
+                        onTap: () {
+                          _onChangeType(type: DateRangeValueType.thisWeek);
+                        },
+                        selected:
+                        rangeValueType == DateRangeValueType.thisWeek),
+                    const SizedBox(width: Dimens.px16),
+                    _buildTag(
+                        title: Strings.tuanTruoc,
+                        onTap: () {
+                          _onChangeType(type: DateRangeValueType.lastWeek);
+                        },
+                        selected:
+                        rangeValueType == DateRangeValueType.lastWeek),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: Dimens.px8),
+                child: KayleeText.normal16W500(
+                  Strings.chonTheoNgay,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Row(
+                children: [
+                  _buildPicker(
+                    date: dateRange.start,
+                    onTap: () {
+                      showKayleeDatePickerDialog(
+                        context: context,
+                        initialDateTime: dateRange.start,
+                        maximumDate: dateRange.end,
+                        onDone: () {
+                          if (selectedStartDate.isNotNull) {
+                            dateRange = DateTimeRange(
+                                start: selectedStartDate, end: dateRange.end);
+                            selectedStartDate = null;
+                            rangeValueType = null;
+                            setState(() {});
+                          }
+                        },
+                        onDismiss: () {
+                          selectedStartDate = null;
+                        },
+                        onDateTimeChanged: (changed) {
+                          selectedStartDate = changed;
+                        },
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    width: Dimens.px8,
+                  ),
+                  _buildPicker(
+                    date: dateRange.end,
+                    onTap: () {
+                      showKayleeDatePickerDialog(
+                        context: context,
+                        initialDateTime: dateRange.end,
+                        maximumDate: DateTime.now(),
+                        onDone: () {
+                          if (selectedEndDate.isNotNull) {
+                            if (selectedEndDate
+                                .difference(dateRange.start)
+                                .inDays <
+                                0) {
+                              dateRange = DateTimeRange(
+                                  start: selectedEndDate, end: selectedEndDate);
+                            } else {
+                              dateRange = DateTimeRange(
+                                  start: dateRange.start, end: selectedEndDate);
+                            }
+                            selectedEndDate = null;
+                            rangeValueType = null;
+                            setState(() {});
+                          }
+                        },
+                        onDismiss: () {
+                          selectedEndDate = null;
+                        },
+                        onDateTimeChanged: (changed) {
+                          selectedEndDate = changed;
+                        },
+                      );
+                    },
+                  ),
+                ],
+              )
+            ],
           ),
         ),
         Container(
           decoration: const BoxDecoration(
               border: Border(
                   top: BorderSide(
-            color: ColorsRes.divider,
-            width: Dimens.px1,
-          ))),
+                    color: ColorsRes.divider,
+                    width: Dimens.px1,
+                  ))),
           padding: const EdgeInsets.all(Dimens.px16),
           child: Row(
             children: [
@@ -56,8 +191,12 @@ class _KayleeDateRangePickerViewState
                   margin: EdgeInsets.zero,
                   text: Strings.luu,
                   onPressed: () {
-                    primaryFocus.unfocus();
                     popScreen();
+                    if (rangeValueType.isNotNull) {
+                      widget.onSelectByType?.call(rangeValueType);
+                    } else {
+                      widget.onSelectByDate?.call(dateRange);
+                    }
                   },
                 ),
               )
@@ -67,14 +206,79 @@ class _KayleeDateRangePickerViewState
       ],
     );
   }
+
+  void _onChangeType({DateRangeValueType type}) {
+    setState(() {
+      rangeValueType = type;
+      dateRange = rangeValueType.range;
+    });
+  }
+
+  Widget _buildTag({String title, VoidCallback onTap, bool selected = false}) {
+    return selected
+        ? KayleeRoundBorder.hyper(
+      borderWidth: Dimens.px2,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: KayleeText.normal12W400(
+          title,
+          maxLines: 1,
+        ),
+      ),
+      onTap: onTap,
+    )
+        : KayleeRoundBorder(
+      borderColor: ColorsRes.textFieldBorder,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: KayleeText.normal12W400(
+          title,
+          maxLines: 1,
+        ),
+      ),
+      onTap: onTap,
+    );
+  }
+
+  Widget _buildPicker({DateTime date, VoidCallback onTap}) {
+    return Expanded(
+      child: KayleeRoundBorder.normal(
+        padding: const EdgeInsets.only(left: Dimens.px12, right: Dimens.px8),
+        child: Row(
+          children: [
+            Expanded(
+              child: KayleeDateTimeText.dayMonthYear(
+                date,
+                textStyle: TextStyles.normal16W400,
+              ),
+            ),
+            Container(
+              width: Dimens.px1,
+              height: Dimens.px40,
+              color: ColorsRes.textFieldBorder,
+              margin: const EdgeInsets.symmetric(
+                vertical: Dimens.px4,
+              ).copyWith(left: Dimens.px12, right: Dimens.px8),
+            ),
+            Image.asset(
+              Images.ic_calendar,
+              width: Dimens.px16,
+              height: Dimens.px16,
+            )
+          ],
+        ),
+        onTap: onTap,
+      ),
+    );
+  }
 }
 
-extension DateRangeValueExtension on DateRangeValue {
+extension DateRangeValueExtension on DateRangeValueType {
   DateTimeRange get range {
     switch (this) {
-      case DateRangeValue.thisWeek:
+      case DateRangeValueType.thisWeek:
         return DateTimeRange(start: DateTime.now(), end: DateTime.now());
-      case DateRangeValue.lastWeek:
+      case DateRangeValueType.lastWeek:
         return DateTimeRange(start: DateTime.now(), end: DateTime.now());
       default:
         return null;
