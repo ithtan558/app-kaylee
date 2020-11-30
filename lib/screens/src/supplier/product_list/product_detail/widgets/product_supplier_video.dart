3717aab804b9085dc6e83f9b1ff62662d482f9c9
tungpham <phamthanhtung1995@gmail.com>
2020-12-01 02:38:36 +0700

    show video

--- lib/screens/src/supplier/product_list/product_detail/widgets/product_supplier_video.dart
+++ lib/screens/src/supplier/product_list/product_detail/widgets/product_supplier_video.dart
@@ -1,7 +1,10 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:anth_package/anth_package.dart';
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kaylee/models/models.dart';
-import 'package:kaylee/res/res.dart';
+import 'package:kaylee/widgets/widgets.dart';
+import 'package:video_player/video_player.dart';
 
 class ProductSupplierVideo extends StatefulWidget {
   final ProductImage image;
@@ -12,21 +15,54 @@ class ProductSupplierVideo extends StatefulWidget {
   _ProductSupplierVideoState createState() => _ProductSupplierVideoState();
 }
 
-class _ProductSupplierVideoState extends State<ProductSupplierVideo> {
+class _ProductSupplierVideoState extends State<ProductSupplierVideo>
+    with AutomaticKeepAliveClientMixin<ProductSupplierVideo> {
+  VideoPlayerController _controller;
+  final _videoStreamController = BehaviorSubject<bool>()..value = false;
+  ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+      widget.image.value ?? '',
+      videoPlayerOptions: VideoPlayerOptions(),
+    )..initialize().then((value) {
+        _videoStreamController.value = _controller.value.initialized;
+        _chewieController = ChewieController(
+          videoPlayerController: _controller,
+          autoPlay: true,
+          deviceOrientationsOnEnterFullScreen: [DeviceOrientation.portraitUp],
+          autoInitialize: true,
+        );
+      });
+  }
+
+  @override
+  void dispose() {
+    _videoStreamController.close();
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      clipBehavior: Clip.antiAlias,
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(Dimens.px10),
-      child: AspectRatio(
-        aspectRatio: 1,
-        child: CachedNetworkImage(
-          imageUrl: widget.image.value ?? '',
-          fit: BoxFit.cover,
-          errorWidget: (context, url, error) => SizedBox(),
-        ),
+    super.build(context);
+    return Container(
+      width: context.screenSize.width,
+      child: StreamBuilder<bool>(
+        stream: _videoStreamController.stream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data)
+            return Chewie(
+              controller: _chewieController,
+            );
+          return Center(child: KayleeLoadingIndicator());
+        },
       ),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
