b31b9fc7b331423063ad85f2d658c56cc10b2b5b
tungpham <phamthanhtung1995@gmail.com>
2020-11-30 19:38:08 +0700

    add order_reason_cancel_id

--- lib/models/src/request/update_order_status/update_order_status_body.g.dart
+++ lib/models/src/request/update_order_status/update_order_status_body.g.dart
@@ -7,8 +7,19 @@ part of 'update_order_status_body.dart';
 // **************************************************************************
 
 Map<String, dynamic> _$UpdateOrderStatusBodyToJson(
-        UpdateOrderStatusBody instance) =>
-    <String, dynamic>{
-      'order_status_id': parseToIntFromOrderStatus(instance.status),
-      'id': instance.id,
-    };
+    UpdateOrderStatusBody instance) {
+  final val = <String, dynamic>{
+    'order_status_id': parseToIntFromOrderStatus(instance.status),
+    'id': instance.id,
+  };
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('order_reason_cancel_id',
+      _orderCancellationReasonToJson(instance.reason));
+  return val;
+}
