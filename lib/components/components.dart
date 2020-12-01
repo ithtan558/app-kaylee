b04dd7935873567bd5f8eafeed0a7db74dc2ff89
tungpham <phamthanhtung1995@gmail.com>
2020-12-01 21:40:59 +0700

    export printer_module.dart

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -3,5 +3,6 @@ library components;
 export 'src/cart/cart_module.dart';
 export 'src/fcm/fcm_module.dart';
 export 'src/networks/network_module.dart';
+export 'src/printer/printer_module.dart';
 export 'src/repositories/repository_module.dart';
 export 'src/user/user_module.dart';
