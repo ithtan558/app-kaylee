de3ae9738944f27160e81f77ee05a94c071806ce
tungpham <phamthanhtung1995@gmail.com>
2020-12-02 11:05:10 +0700

    add `CreateOrderResult`

---
+++ lib/models/src/response/order/result/create_order_result.dart
@@ -0,0 +1,77 @@
+import 'package:core_plugin/core_plugin.dart';
+
+part 'create_order_result.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+class CreateOrderResult {
+  factory CreateOrderResult.fromJson(Map<String, dynamic> json) =>
+      _$CreateOrderResultFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CreateOrderResultToJson(this);
+
+  CreateOrderResult({
+    this.clientId,
+    this.brandId,
+    this.employeeId,
+    this.customerId,
+    this.orderStatusId,
+    this.isPaid,
+    this.supplierId,
+    this.name,
+    this.phone,
+    this.note,
+    this.amount,
+    this.discount,
+    this.createdBy,
+    this.orderId,
+  });
+
+  final int clientId;
+  final int brandId;
+  final int employeeId;
+  final int customerId;
+  final int orderStatusId;
+  final int isPaid;
+  final int supplierId;
+  final String name;
+  final String phone;
+  final String note;
+  final int amount;
+  final int discount;
+  final int createdBy;
+  @JsonKey(name: 'id')
+  final int orderId;
+
+  CreateOrderResult copyWith({
+    int clientId,
+    int brandId,
+    int employeeId,
+    int customerId,
+    int orderStatusId,
+    int isPaid,
+    int supplierId,
+    String name,
+    String phone,
+    String note,
+    int amount,
+    int discount,
+    int createdBy,
+    int orderId,
+  }) =>
+      CreateOrderResult(
+        clientId: clientId ?? this.clientId,
+        brandId: brandId ?? this.brandId,
+        employeeId: employeeId ?? this.employeeId,
+        customerId: customerId ?? this.customerId,
+        orderStatusId: orderStatusId ?? this.orderStatusId,
+        isPaid: isPaid ?? this.isPaid,
+        supplierId: supplierId ?? this.supplierId,
+        name: name ?? this.name,
+        phone: phone ?? this.phone,
+        note: note ?? this.note,
+        amount: amount ?? this.amount,
+        discount: discount ?? this.discount,
+        createdBy: createdBy ?? this.createdBy,
+        orderId: orderId ?? this.orderId,
+      );
+}
---
+++ lib/models/src/response/order/result/create_order_result.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'create_order_result.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CreateOrderResult _$CreateOrderResultFromJson(Map<String, dynamic> json) {
+  return CreateOrderResult(
+    clientId: json['client_id'] as int,
+    brandId: json['brand_id'] as int,
+    employeeId: json['employee_id'] as int,
+    customerId: json['customer_id'] as int,
+    orderStatusId: json['order_status_id'] as int,
+    isPaid: json['is_paid'] as int,
+    supplierId: json['supplier_id'] as int,
+    name: json['name'] as String,
+    phone: json['phone'] as String,
+    note: json['note'] as String,
+    amount: json['amount'] as int,
+    discount: json['discount'] as int,
+    createdBy: json['created_by'] as int,
+    orderId: json['id'] as int,
+  );
+}
+
+Map<String, dynamic> _$CreateOrderResultToJson(CreateOrderResult instance) =>
+    <String, dynamic>{
+      'client_id': instance.clientId,
+      'brand_id': instance.brandId,
+      'employee_id': instance.employeeId,
+      'customer_id': instance.customerId,
+      'order_status_id': instance.orderStatusId,
+      'is_paid': instance.isPaid,
+      'supplier_id': instance.supplierId,
+      'name': instance.name,
+      'phone': instance.phone,
+      'note': instance.note,
+      'amount': instance.amount,
+      'discount': instance.discount,
+      'created_by': instance.createdBy,
+      'id': instance.orderId,
+    };
