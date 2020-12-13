e73993aacd013cdcb73fa01b55c44072ccaad1bc
tungpham <phamthanhtung1995@gmail.com>
2020-12-13 22:43:48 +0700

    remove `parseToIntFromOrderStatus` and `parseOrderStatusFromInt`

--- lib/models/src/response/commission/order/commission_order.g.dart
+++ lib/models/src/response/commission/order/commission_order.g.dart
@@ -12,7 +12,9 @@ CommissionOrder _$CommissionOrderFromJson(Map<String, dynamic> json) {
     code: json['code'] as String,
     name: json['name'] as String,
     amount: json['amount'] as int,
-    orderStatus: parseOrderStatusFromInt(json['order_status_id']),
+    orderStatus: _$enumDecodeNullable(
+        _$OrderStatusEnumMap, json['order_status_id'],
+        unknownValue: OrderStatus.unknown),
     createdAt: json['created_at'] as String,
     supplierName: json['supplier_name'] as String,
     commissionProduct: json['commission_product'] as int,
@@ -26,9 +28,53 @@ Map<String, dynamic> _$CommissionOrderToJson(CommissionOrder instance) =>
       'code': instance.code,
       'name': instance.name,
       'amount': instance.amount,
-      'order_status_id': parseToIntFromOrderStatus(instance.orderStatus),
+      'order_status_id': _$OrderStatusEnumMap[instance.orderStatus],
       'created_at': instance.createdAt,
       'supplier_name': instance.supplierName,
       'commission_product': instance.commissionProduct,
       'commission_service': instance.commissionService,
     };
+
+T _$enumDecode<T>(
+  Map<T, dynamic> enumValues,
+  dynamic source, {
+  T unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError('A value must be provided. Supported values: '
+        '${enumValues.values.join(', ')}');
+  }
+
+  final value = enumValues.entries
+      .singleWhere((e) => e.value == source, orElse: () => null)
+      ?.key;
+
+  if (value == null && unknownValue == null) {
+    throw ArgumentError('`$source` is not one of the supported values: '
+        '${enumValues.values.join(', ')}');
+  }
+  return value ?? unknownValue;
+}
+
+T _$enumDecodeNullable<T>(
+  Map<T, dynamic> enumValues,
+  dynamic source, {
+  T unknownValue,
+}) {
+  if (source == null) {
+    return null;
+  }
+  return _$enumDecode<T>(enumValues, source, unknownValue: unknownValue);
+}
+
+const _$OrderStatusEnumMap = {
+  OrderStatus.ordered: 1,
+  OrderStatus.waiting: 2,
+  OrderStatus.finished: 3,
+  OrderStatus.not_paid: 4,
+  OrderStatus.cancel: 5,
+  OrderStatus.accepted: 6,
+  OrderStatus.refund: 7,
+  OrderStatus.refundSalon: 8,
+  OrderStatus.unknown: null,
+};
