7b01d03516a85ac0b54652c2030811e7809697b3
tungpham <phamthanhtung1995@gmail.com>
2021-03-04 20:53:23 +0700

    add `hoTen`

--- lib/res/src/strings.dart
+++ lib/res/src/strings.dart
@@ -209,6 +209,7 @@ class Strings {
   static const String nhapGhiChuCuaKH = 'Nhập ghi chú của khách hàng';
   static const String banChuaChonSanPham = 'Bạn chưa chọn sản phẩm nào.';
   static const String hoVaTen = 'Họ & Tên';
+  static const String hoTen = 'Họ Tên';
   static const String nhapHoVaTen = 'Nhập Họ và Tên';
   static const String hoanTat = 'Hoàn tất';
   static const String xinVuiLongChonTinh = 'Xin vui lòng chọn Tỉnh/Thành phố';
