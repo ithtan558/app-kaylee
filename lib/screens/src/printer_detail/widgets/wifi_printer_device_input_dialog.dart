737d4ed432b4f222e128b1383f0667a27113d4e5
tungpham <phamthanhtung1995@gmail.com>
2021-03-13 19:53:59 +0700

    create `WifiPrinterDeviceInputDialog`

---
+++ lib/screens/src/printer_detail/widgets/wifi_printer_device_input_dialog.dart
@@ -0,0 +1,58 @@
+import 'package:anth_package/anth_package.dart';
+import 'package:flutter/material.dart';
+import 'package:kaylee/base/kaylee_state.dart';
+import 'package:kaylee/res/res.dart';
+import 'package:kaylee/widgets/widgets.dart';
+
+class WifiPrinterDeviceInputDialog extends StatefulWidget {
+  final ValueSetter<String> onSave;
+
+  WifiPrinterDeviceInputDialog({this.onSave});
+
+  @override
+  _WifiPrinterDeviceInputDialogState createState() =>
+      _WifiPrinterDeviceInputDialogState();
+}
+
+class _WifiPrinterDeviceInputDialogState
+    extends KayleeState<WifiPrinterDeviceInputDialog> {
+  final ipTfController = TextEditingController();
+
+  @override
+  void dispose() {
+    ipTfController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KayleeAlertDialogView(
+      title: Strings.ketNoiVoiMayIn,
+      contentWidget: Padding(
+        padding: const EdgeInsets.only(top: Dimens.px8),
+        child: Material(
+          clipBehavior: Clip.antiAlias,
+          borderRadius: BorderRadius.circular(Dimens.px5),
+          child: KayleeTextField.normal(
+            hint: Strings.ipHint,
+            controller: ipTfController,
+            maxLength: 15,
+          ),
+        ),
+      ),
+      actions: [
+        KayleeAlertDialogAction.huy(
+          onPressed: context.pop,
+        ),
+        KayleeAlertDialogAction(
+          title: Strings.luu,
+          isDefaultAction: true,
+          onPressed: () {
+            widget.onSave?.call(ipTfController.text);
+            context.pop();
+          },
+        ),
+      ],
+    );
+  }
+}
