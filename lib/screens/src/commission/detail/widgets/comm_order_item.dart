b4fe28c22b5cdb3d0f4cfca86f8fc626dc592e25
tungpham <phamthanhtung1995@gmail.com>
2021-03-28 12:52:10 +0700

    update layout

--- lib/screens/src/commission/detail/widgets/comm_order_item.dart
+++ lib/screens/src/commission/detail/widgets/comm_order_item.dart
@@ -18,33 +18,30 @@ class CommOrderItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: Dimens.px103,
+    return Material(
       clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.fromBorderSide(BorderSide(
+      color: ColorsRes.textFieldBorder,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(Dimens.px5),
+        side: BorderSide(
           color: ColorsRes.textFieldBorder,
           width: Dimens.px1,
-        )),
-        borderRadius: BorderRadius.circular(Dimens.px5),
+        ),
       ),
       child: Row(
         children: [
-          AspectRatio(
-            aspectRatio: 1,
-            child: Container(
-              color: ColorsRes.textFieldBorder,
-              alignment: Alignment.center,
-              child: KayleeText.normal18W700(
-                date?.toFormatString(pattern: 'dd/MM') ?? '',
-                maxLines: 1,
-                textAlign: TextAlign.center,
-              ),
+          Container(
+            alignment: Alignment.center,
+            width: Dimens.px96,
+            child: KayleeText.normal18W700(
+              date?.toFormatString(pattern: 'dd/MM') ?? '',
+              maxLines: 1,
+              textAlign: TextAlign.center,
             ),
           ),
           Expanded(
-            child: Padding(
+            child: Container(
+              color: Colors.white,
               padding: const EdgeInsets.all(Dimens.px16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -55,15 +52,16 @@ class CommOrderItem extends StatelessWidget {
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
                   ),
-                  KayleeText.hint16W400(
-                    name ?? '',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: Dimens.px8),
+                    child: KayleeText.hint16W400(
+                      name ?? '',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
                   KayleeText.hyper16W400(
-                    '${Strings.hoaHong}: ${CurrencyUtils.formatVNDWithCustomUnit(commissionAmount, unit: Strings.vnd)} ',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
+                    '${Strings.hoaHong}: ${CurrencyUtils.formatVNDWithCustomUnit(commissionAmount)} ',
                   ),
                 ],
               ),
