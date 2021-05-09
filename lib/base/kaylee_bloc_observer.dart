d8376c32bc76bc91e59b4bc943f9c3c4bfafca9b
TungPT95 <tung@shopform.sg>
2021-05-09 15:35:59 +0700

    migrate-flutter-2

--- lib/base/kaylee_bloc_observer.dart
+++ lib/base/kaylee_bloc_observer.dart
@@ -2,22 +2,22 @@ import 'package:anth_package/anth_package.dart';
 
 class KayleeBlocObserver extends BlocObserver {
   //khi bloc extend Bloc
-  final void Function(dynamic currentState, dynamic nextState) transition;
+  final void Function(dynamic currentState, dynamic nextState)? transition;
 
   //khi bloc extend Cubit
-  final void Function(Cubit cubit, Change change) change;
+  final void Function(BlocBase cubit, Change change)? change;
 
   KayleeBlocObserver({this.transition, this.change});
 
   @override
-  void onTransition(Cubit cubit, Transition transition) {
-    this.transition?.call(transition.currentState, transition?.nextState);
-    super.onTransition(cubit, transition);
+  void onTransition(Bloc bloc, Transition transition) {
+    this.transition?.call(transition.currentState, transition.nextState);
+    super.onTransition(bloc, transition);
   }
 
   @override
-  void onChange(Cubit cubit, Change change) {
-    this.change?.call(cubit, change);
-    super.onChange(cubit, change);
+  void onChange(BlocBase bloc, Change change) {
+    this.change?.call(bloc, change);
+    super.onChange(bloc, change);
   }
 }
