8a68bdd6b51f6ac2ab82e5f8237cae508b9973ec
TungPT95 <tung@shopform.sg>
2021-05-09 20:24:48 +0700

    migrate-flutter-2

--- lib/base/kaylee_observer.dart
+++ lib/base/kaylee_observer.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 
 class KayleeObserver extends NavigatorObserver {
   @override
-  void didPush(Route<dynamic> route, Route<dynamic> previousRoute) {
-    // print('[TUNG] ===> route ${route is PopupRoute}');
-    // print('[TUNG] ===> route.name ${route.settings.name}');
+  void didPush(Route route, Route? previousRoute) {
+    super.didPush(route, previousRoute);
   }
 
   @override
-  void didPop(Route route, Route previousRoute) {
-    // print('[TUNG] ===> didPop route ${route is PopupRoute}');
-    // print('[TUNG] ===> didPop route.name ${route.settings.name}');
+  void didPop(Route route, Route? previousRoute) {
+    super.didPop(route, previousRoute);
   }
 
   @override
-  void didRemove(Route route, Route previousRoute) {}
+  void didRemove(Route route, Route? previousRoute) {
+    super.didRemove(route, previousRoute);
+  }
 
   @override
-  void didReplace({Route newRoute, Route oldRoute}) {}
+  void didReplace({Route? newRoute, Route? oldRoute}) {
+    super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
+  }
 }
