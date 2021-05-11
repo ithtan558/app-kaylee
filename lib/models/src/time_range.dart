22fd2abe6184afe6cd2ee1597a9d1c0f6eab8e70
TungPT95 <tung@shopform.sg>
2021-05-12 00:14:07 +0700

    migrate-flutter-2 time_range.dart

--- lib/models/src/time_range.dart
+++ lib/models/src/time_range.dart
@@ -3,25 +3,25 @@ import 'package:anth_package/anth_package.dart';
 const String _timeFormat = 'HH:mm';
 
 abstract class _RangTime {
-  String time;
+  String? time;
 
   _RangTime({this.time});
 
-  String get formattedTime => time.isNullOrEmpty
+  String? get formattedTime => time == null || time!.isEmpty
       ? null
-      : DateFormat(_timeFormat).format(DateFormat(_timeFormat).parse(time));
+      : DateFormat(_timeFormat).format(DateFormat(_timeFormat).parse(time!));
 
-  DateTime get datetime {
-    if (time.isNullOrEmpty) return null;
-    final date = DateFormat(_timeFormat).parse(this.time);
+  DateTime? get datetime {
+    if (time == null || time!.isEmpty) return null;
+    final date = DateFormat(_timeFormat).parse(time!);
     return date;
   }
 }
 
 class StartTime extends _RangTime {
-  StartTime({String time}) : super(time: time);
+  StartTime({String? time}) : super(time: time);
 }
 
 class EndTime extends _RangTime {
-  EndTime({String time}) : super(time: time);
+  EndTime({String? time}) : super(time: time);
 }
