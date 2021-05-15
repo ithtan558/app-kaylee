7372629d375768e5864ba41cc81192257c87a83c
TungPT95 <tung@shopform.sg>
2021-05-15 19:47:03 +0700

    migrate-flutter-2 deeplink_helper.dart

--- lib/utils/deeplink_helper.dart
+++ lib/utils/deeplink_helper.dart
@@ -5,55 +5,60 @@ import 'package:kaylee/screens/screens.dart';
 class DeepLinkHelper {
   DeepLinkHelper._();
 
-  static PageIntent handleLink({String link}) {
-    final uri = Uri.tryParse(link);
-    if (uri.isNotNull) {
-      if (uri.path == '/supplier/order/detail') {
-        final orderId = int.tryParse(uri.queryParameters['order_id']);
-        if (orderId.isNotNull) {
-          return PageIntent(
-            screen: MyOrderDetailScreen,
-            bundle: Bundle(
-              models.Order(id: orderId),
-            ),
-          );
-        }
-      } else if (uri.path == '/product/detail') {
-        final productId = int.tryParse(uri.queryParameters['product_id']);
-        final supplierId = int.tryParse(uri.queryParameters['supplier_id']);
-        if (productId.isNotNull && supplierId.isNotNull) {
-          return PageIntent(
-            screen: SupplierProductDetailScreen,
-            bundle: Bundle(
-              SupplierProductDetailScreenData(
-                product: models.Product(id: productId),
-                supplier: models.Supplier(id: supplierId),
-                openFrom: ProductDetailScreenOpenFrom.notification,
+  static PageIntent? handleLink({String? link}) {
+    if (link != null && link.isNotEmpty) {
+      final uri = Uri.tryParse(link);
+      if (uri != null) {
+        if (uri.path == '/supplier/order/detail') {
+          final orderId = int.tryParse(uri.queryParameters['order_id'] ?? '');
+          if (orderId != null) {
+            return PageIntent(
+              screen: MyOrderDetailScreen,
+              bundle: Bundle(
+                models.Order(id: orderId),
               ),
-            ),
-          );
-        }
-      } else if (uri.path == '/notification/detail') {
-        final notificationId =
-            int.tryParse(uri.queryParameters['notification_id']);
-        if (notificationId.isNotNull) {
-          return PageIntent(
-            screen: NotifyDetailScreen,
-            bundle: Bundle(
-              models.Notification(
-                id: notificationId,
-                status: models.NotificationStatus.notRead,
+            );
+          }
+        } else if (uri.path == '/product/detail') {
+          final productId =
+              int.tryParse(uri.queryParameters['product_id'] ?? '');
+          final supplierId =
+              int.tryParse(uri.queryParameters['supplier_id'] ?? '');
+          if (productId != null && supplierId != null) {
+            return PageIntent(
+              screen: SupplierProductDetailScreen,
+              bundle: Bundle(
+                SupplierProductDetailScreenData(
+                  product: models.Product(id: productId),
+                  supplier: models.Supplier(id: supplierId),
+                  openFrom: ProductDetailScreenOpenFrom.notification,
+                ),
               ),
-            ),
-          );
-        }
-      } else if (uri.path == '/supplier') {
-        final supplierId = int.tryParse(uri.queryParameters['supplier_id']);
-        if (supplierId.isNotNull) {
-          return PageIntent(
-            screen: SupplierProdListScreen,
-            bundle: Bundle(models.Supplier(id: supplierId)),
-          );
+            );
+          }
+        } else if (uri.path == '/notification/detail') {
+          final notificationId =
+              int.tryParse(uri.queryParameters['notification_id'] ?? '');
+          if (notificationId != null) {
+            return PageIntent(
+              screen: NotifyDetailScreen,
+              bundle: Bundle(
+                models.Notification(
+                  id: notificationId,
+                  status: models.NotificationStatus.notRead,
+                ),
+              ),
+            );
+          }
+        } else if (uri.path == '/supplier') {
+          final supplierId =
+              int.tryParse(uri.queryParameters['supplier_id'] ?? '');
+          if (supplierId != null) {
+            return PageIntent(
+              screen: SupplierProdListScreen,
+              bundle: Bundle(models.Supplier(id: supplierId)),
+            );
+          }
         }
       }
     }
@@ -61,8 +66,8 @@ class DeepLinkHelper {
   }
 
   ///[ifNOtFound]: khi [link] ko xác định đc trang cần navigate tới => return screen đc chỉ định
-  static PageIntent handleNotificationLink(
-      {String link, PageIntent ifNOtFound}) {
+  static PageIntent? handleNotificationLink(
+      {String? link, PageIntent? ifNOtFound}) {
     return handleLink(link: link) ?? ifNOtFound;
   }
 }
