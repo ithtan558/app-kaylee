97db14d32444efd2adcd8f60a08e37419671a1ee
TungPT95 <tung@shopform.sg>
2021-05-30 00:57:56 +0700

    only show error if not null

--- lib/screens/src/brand/create_new/create_new_brand_screen.dart
+++ lib/screens/src/brand/create_new/create_new_brand_screen.dart
@@ -58,7 +58,9 @@ class _CreateNewBrandScreenState extends KayleeState<CreateNewBrandScreen> {
         showLoading();
       } else if (!state.loading) {
         hideLoading();
-        if (state.code.isNotNull && state.code != ErrorType.UNAUTHORIZED) {
+        if (state.code.isNotNull &&
+            state.code != ErrorType.UNAUTHORIZED &&
+            state.error != null) {
           showKayleeAlertErrorYesDialog(
             context: context,
             error: state.error,
