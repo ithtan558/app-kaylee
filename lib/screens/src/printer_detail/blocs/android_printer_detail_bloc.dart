d90911a5334ae56804b73879146ee314574a526e
TungPT95 <tung@shopform.sg>
2021-06-13 20:28:50 +0700

    * dont call `startScanBluetoothDevice` in `AndroidPrinterDetailBloc`

--- lib/screens/src/printer_detail/blocs/android_printer_detail_bloc.dart
+++ lib/screens/src/printer_detail/blocs/android_printer_detail_bloc.dart
@@ -3,4 +3,18 @@ part of 'base/printer_detail_base.dart';
 class AndroidPrinterDetailBloc extends PrinterDetailBase
     with BluetoothPrinterMixin, AndroidBluetoothPrinterMixin, WifiPrinterMixin {
   AndroidPrinterDetailBloc() : super._();
+
+  @override
+  void initState() async {
+    emit(PrinterDetailStateLoadingDevices());
+    final fromSharePref = SharedRef.getString(PRINTER_DEVICES_KEY);
+    final map =
+        jsonDecode(fromSharePref.isNullOrEmpty ? '[]' : fromSharePref) as List;
+    devices = map.map((e) => PrinterDevice.fromJson(e)).toList();
+    emit(PrinterDetailStateLoadedDevices());
+  }
+}
+
+class AndroidPrinterDialogBloc extends AndroidPrinterDetailBloc {
+  AndroidPrinterDialogBloc() : super();
 }
