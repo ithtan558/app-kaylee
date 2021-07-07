a20416111064e931beaaabfaebce3bf24d03bf05
TungPT95 <tung@shopform.sg>
2021-07-07 12:58:37 +0700

    migrate-flutter-2: call Firebase.initializeApp

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,12 +1,11 @@
-import 'package:anth_package/anth_package.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kaylee/application_config.dart';
-import 'package:kaylee/base/json_converter/kaylee_json_convert.dart';
 import 'package:kaylee/kaylee_application.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  JsonConverterBuilder.init(KayleeJsonConverter());
+  await Firebase.initializeApp();
   runApp(KayLeeApplication.newInstance(appConfig: DevelopmentAppConfig()));
 }
