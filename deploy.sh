53264ce27a12af5e8291e62057d298e873650de2
tungpham <phamthanhtung1995@gmail.com>
2021-08-29 21:36:56 +0700

    * update script * rename file

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+flavor='dev'
+
+run_fastlane() {
+  bundle exec fastlane $1
+}
+
+select_flavor(){
+  echo "Select the flavor:"
+  echo "1: dev"
+  echo "2: prod"
+  read value
+
+
+  if [ $value -eq 1 ]
+  then
+    flavor="dev"
+  elif [ $value -eq 2 ]
+  then
+    flavor="prod"
+  fi
+}
+
+deploy(){
+  select_flavor
+  cd ios
+  run_fastlane $flavor
+  cd ../android
+  run_fastlane $flavor
+}
+
+deploy
+
+
--- dev_deploy.sh
+++
@@ -1,4 +0,0 @@
-cd ios
-bundle exec fastlane dev
-cd ../android
-bundle exec fastlane dev
