333e56ea660e9e2fb117f8d30b655a55b06b195f
tungpham <phamthanhtung1995@gmail.com>
2021-09-16 01:01:34 +0700

    update fastlane

--- deploy.sh
+++ deploy.sh
@@ -13,32 +13,30 @@ you_re_here="You're here: "
 flavor='dev'
 
 run_fastlane() {
-  bundle exec fastlane $1
+    bundle exec fastlane $1
 }
 
 select_flavor() {
-  echo "Welcome to the show\n"
-  echo -e "${red}${bold}[1]${reset}${green}: [dev] Development${reset}"
-  echo "${yellow}${bold}[2]${reset}${green}: [prod] Production${reset}"
-  printf "${green}\nPlease select your flavor, default is ${magentaBg}${white}[dev]${reset} (Press ${white}${yellowBg}enter${reset} to use the default): ${reset}"
-  read value
+    echo "Welcome to the show\n"
+    echo -e "${red}${bold}[1]${reset}${green}: [dev] Development${reset}"
+    echo "${yellow}${bold}[2]${reset}${green}: [prod] Production${reset}"
+    printf "${green}\nPlease select your flavor, default is ${magentaBg}${white}[dev]${reset} (Press ${white}${yellowBg}enter${reset} to use the default): ${reset}"
+    read value
 
-  if [ "$value" = "2" ]
-  then
-    flavor="prod"
-  fi
+    if [ "$value" = "2" ]
+    then
+      flavor="prod"
+    fi
 }
 
 deploy() {
-  select_flavor
-  cd ios
-  echo "${you_re_here}${white}${bold}${greenBg}$(pwd)${reset}"
-  run_fastlane $flavor
-  cd ../android
-  echo "${you_re_here}${white}${bold}${greenBg}$(pwd)${reset}"
-  run_fastlane $flavor
+    select_flavor
+    cd ios
+    echo "${you_re_here}${white}${bold}${greenBg}$(pwd)${reset}"
+    run_fastlane $flavor
+    cd ../android
+    echo "${you_re_here}${white}${bold}${greenBg}$(pwd)${reset}"
+    run_fastlane $flavor
 }
 
 deploy
-
-
