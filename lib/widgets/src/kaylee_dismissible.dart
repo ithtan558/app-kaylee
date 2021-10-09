2137b239930f97519c46274c46634de90ce633e8
tungpham <phamthanhtung1995@gmail.com>
2021-10-09 23:17:54 +0700

    refactor import

--- lib/widgets/src/kaylee_dismissible.dart
+++ lib/widgets/src/kaylee_dismissible.dart
@@ -2,7 +2,6 @@ import 'package:anth_package/anth_package.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kaylee/res/res.dart';
-import 'package:kaylee/widgets/widgets.dart';
 
 class KayleeDismissible extends StatelessWidget {
   final Widget child;
