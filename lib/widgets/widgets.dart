c05b4e32619bfad446d9586d1e4d9bd20790fd84
tungpham <phamthanhtung1995@gmail.com>
2021-10-09 23:33:08 +0700

    move `kaylee_round_border.dart` to `anth_package`

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -40,7 +40,6 @@ export 'src/kaylee_price_range.dart';
 export 'src/kaylee_prod_item.dart';
 export 'src/kaylee_quantity_slider.dart';
 export 'src/kaylee_refresh_indicator.dart';
-export 'src/kaylee_round_border.dart';
 export 'src/kaylee_rounded_button.dart';
 export 'src/kaylee_scrollview.dart';
 export 'src/kaylee_tab_view/kaylee_tab_view.dart';
