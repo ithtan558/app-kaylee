00881d99b677ed95208076d05547f10a24ffb8be
tungpham <phamthanhtung1995@gmail.com>
2021-10-09 23:34:05 +0700

    move `kaylee_quantity_slider.dart` to `anth_package`

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -38,7 +38,6 @@ export 'src/kaylee_network_image.dart';
 export 'src/kaylee_picker_textfield.dart';
 export 'src/kaylee_price_range.dart';
 export 'src/kaylee_prod_item.dart';
-export 'src/kaylee_quantity_slider.dart';
 export 'src/kaylee_refresh_indicator.dart';
 export 'src/kaylee_rounded_button.dart';
 export 'src/kaylee_scrollview.dart';
