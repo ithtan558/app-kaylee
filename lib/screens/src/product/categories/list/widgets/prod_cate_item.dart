f7da2b4fc5f6c484700d54fff556b5ece3c9d85d
tungpham <phamthanhtung1995@gmail.com>
2021-10-10 17:08:28 +0700

    move `category_item.dart` to `anth_package`

--- lib/screens/src/product/categories/list/widgets/prod_cate_item.dart
+++ lib/screens/src/product/categories/list/widgets/prod_cate_item.dart
@@ -1,14 +1,13 @@
+import 'package:anth_package/anth_package.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:kaylee/models/models.dart';
-import 'package:kaylee/widgets/widgets.dart';
 
 class ProdCateItem extends CategoryItem {
-  ProdCateItem(
-      {Key? key, required ProdCate category, required VoidCallback onTap})
+  ProdCateItem({Key? key, required ProdCate category, required VoidCallback onTap})
       : super(
-          key: key,
-          index: category.sequence,
-          name: category.name,
-          onTap: onTap,
-        );
+    key: key,
+    index: category.sequence,
+    name: category.name,
+    onTap: onTap,
+  );
 }
--- lib/screens/src/service/categories/list/widgets/serv_cate_item.dart
+++ lib/screens/src/service/categories/list/widgets/serv_cate_item.dart
@@ -1,14 +1,13 @@
+import 'package:anth_package/anth_package.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:kaylee/models/models.dart';
-import 'package:kaylee/widgets/widgets.dart';
 
 class ServCateItem extends CategoryItem {
-  ServCateItem(
-      {Key? key, required ServiceCate category, required VoidCallback onTap})
+  ServCateItem({Key? key, required ServiceCate category, required VoidCallback onTap})
       : super(
-          index: category.sequence,
-          name: category.name,
-          onTap: onTap,
-          key: key,
-        );
+    index: category.sequence,
+    name: category.name,
+    onTap: onTap,
+    key: key,
+  );
 }
