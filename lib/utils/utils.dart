4756f205ec66c47bc6513bdadc64f23c2c13844b
tungpham <phamthanhtung1995@gmail.com>
2021-10-13 21:30:56 +0700

    add `utils.dart` to `anth_package`

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,8 +2,3 @@ library utils;
 
 export 'src/extensions.dart';
 export 'src/model_extensions.dart';
-
-const String dateFormat = 'yyyy-MM-dd';
-const String dateFormat1 = 'yyyy-MM-dd HH:mm:ss';
-const String dateFormat2 = 'dd/MM/yyyy';
-const String dateFormat3 = 'HH:mm';
