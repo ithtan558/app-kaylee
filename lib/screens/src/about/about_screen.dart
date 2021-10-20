0e7d91f221008a25d839245d54e1d4c33144e470
tungpham <phamthanhtung1995@gmail.com>
2021-10-20 21:19:16 +0700

    optimize import

--- lib/screens/src/about/about_screen.dart
+++ lib/screens/src/about/about_screen.dart
@@ -1,5 +1,4 @@
 import 'package:anth_package/anth_package.dart';
-import 'package:anth_package/src/widgets/kaylee_appbar.dart';
 import 'package:core_plugin/core_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:kaylee/res/res.dart';
