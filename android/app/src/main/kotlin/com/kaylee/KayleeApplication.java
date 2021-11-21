1e100bee6468ef19ca646d4f9ff1daa9e8020254
tungpham <phamthanhtung1995@gmail.com>
2021-11-21 23:39:36 +0700

    remove `FlutterFirebaseMessagingBackgroundService.setPluginRegistrant`

--- android/app/src/main/kotlin/com/kaylee/KayleeApplication.java
+++ android/app/src/main/kotlin/com/kaylee/KayleeApplication.java
@@ -1,17 +1,6 @@
 package com.kaylee;
 
 import io.flutter.app.FlutterApplication;
-import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingBackgroundService;
 
-public class KayleeApplication extends FlutterApplication implements PluginRegistry.PluginRegistrantCallback {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        FlutterFirebaseMessagingBackgroundService.setPluginRegistrant(this);
-    }
-
-    @Override
-    public void registerWith(PluginRegistry registry) {
-    }
+public class KayleeApplication extends FlutterApplication {
 }
